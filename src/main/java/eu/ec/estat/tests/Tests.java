4b1d893f0df5096f169d360b7eb59b087073c881
jgaffuri <julien.gaffuri@ec.europa.eu>
2017-02-08 10:51:25 +0100

    initial

---
+++ src/main/java/eu/ec/estat/tests/Tests.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package eu.ec.estat.tests;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class Tests {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		//load databases for tourism
+		//load NUTS regions
+		//
+		
+		
+		
+		
+		
+	}
+
+}
