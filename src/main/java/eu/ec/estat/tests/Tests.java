322634b89ac7ca69148da1cf8168535351cc2846
jgaffuri <julien.gaffuri@ec.europa.eu>
2017-02-10 16:11:36 +0100

    0

--- src/main/java/eu/ec/estat/tests/Tests.java
+++ src/main/java/eu/ec/estat/tests/Tests.java
@@ -3,6 +3,10 @@
  */
 package eu.ec.estat.tests;
 
+import eu.ec.estat.java4eurostat.base.StatsHypercube;
+import eu.ec.estat.java4eurostat.io.EurobaseIO;
+import eu.ec.estat.java4eurostat.io.EurostatTSV;
+
 /**
  * @author julien Gaffuri
  *
@@ -10,18 +14,20 @@ package eu.ec.estat.tests;
 public class Tests {
 
 	public static void main(String[] args) {
-		
-		
-		
-		
-		//load databases for tourism
+
+
+
+
+		//download/update data for tourism
+		EurobaseIO.update("H:/eurobase/", "tour_occ_nim", "tour_occ_nin2", "tour_occ_nin2d", "tour_occ_nin2c");
+
+		System.out.println("Loading...");
+		StatsHypercube hc = EurostatTSV.load("H:/eurobase/tour_occ_nin2.tsv");
+		hc.printInfo();
+
 		//load NUTS regions
 		//
-		
-		
-		
-		
-		
+
 	}
 
 }
