1fadcf7e80f618623bfe75ac59036d26be62b482
jgaffuri <julien.gaffuri@ec.europa.eu>
2017-03-01 11:54:21 +0100

    0

--- src/main/java/eu/ec/estat/geostat/TourismUseCase.java
+++ src/main/java/eu/ec/estat/geostat/TourismUseCase.java
@@ -3,24 +3,12 @@
  */
 package eu.ec.estat.geostat;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.util.Collection;
 import java.util.HashMap;
 
-import javax.imageio.ImageIO;
-
-import org.geotools.geometry.jts.ReferencedEnvelope;
-import org.geotools.map.MapContent;
-import org.geotools.renderer.GTRenderer;
-import org.geotools.renderer.lite.StreamingRenderer;
-
 import eu.ec.estat.geostat.dasymetric.DasymetricMapping;
 import eu.ec.estat.geostat.io.ShapeFile;
+import eu.ec.estat.geostat.mapping.NUTSMap;
 import eu.ec.estat.java4eurostat.analysis.Validation;
 import eu.ec.estat.java4eurostat.base.Selection;
 import eu.ec.estat.java4eurostat.base.Stat;
@@ -40,9 +28,6 @@ public class TourismUseCase {
 	public static String POI_TOURISEM_SHP_BASE = BASE_PATH + "eur2016_12/mnpoi_";
 
 
-	//TODO validation with E4 figures
-	//futher analyse maximum errors
-	//better analyse validation results: show on map !
 	//TODO show maps - make generic library
 
 	//TODO aggregate at 10km grid level
@@ -59,7 +44,7 @@ public class TourismUseCase {
 
 		//runDasymetric();
 		//computeValidation();
-		//produceMaps();
+		new NUTSMap().produce();
 
 		//E4 data validation
 		//filterE4ValidationDataAggregatesNUTS2();
@@ -172,99 +157,14 @@ public class TourismUseCase {
 	}
 
 
-	private static void produceMaps() {
-		//show results on maps
-
-		/*/produce map
-		//https://github.com/geotools/geotools/blob/master/docs/src/main/java/org/geotools/tutorial/style/StyleLab.java
-		//http://docs.geotools.org/latest/userguide/library/render/gtrenderer.html
-		//http://docs.geotools.org/latest/userguide/library/render/index.html
-		//http://gis.stackexchange.com/questions/123903/how-to-create-a-map-and-save-it-to-an-image-with-geotools
-
-		// Create a map content and add our shapefile to it
-		MapContent map = new MapContent();
-		map.setTitle("NUTS Tourism");
-		//MapViewport vp = new MapViewport(new ReferencedEnvelope(-5.0, 18.0, 42.0, 55.0, shpFileNUTS.getCRS() ));
-		//vp.setCoordinateReferenceSystem(CRS.decode("EPSG:3035"));
-		//map.setViewport(vp);
-		MapViewport vp = map.getViewport();
-		vp.setCoordinateReferenceSystem(CRS.decode("EPSG:3035"));
-		vp.setBounds(new ReferencedEnvelope(2500000.0,5600000.0,1700000.0,4700000.0, CRS.decode("EPSG:3035") ));
-
-		//style
-		// create a partially opaque outline stroke
-		StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory();
-		FilterFactory filterFactory = CommonFactoryFinder.getFilterFactory();
-
-		Stroke stroke = styleFactory.createStroke(
-				filterFactory.literal(Color.WHITE),
-				filterFactory.literal(1),
-				filterFactory.literal(0.5));
-
-		// create a partial opaque fill
-		Fill fill = styleFactory.createFill(
-				filterFactory.literal(Color.gray),
-				filterFactory.literal(0.5));
-		PolygonSymbolizer sym = styleFactory.createPolygonSymbolizer(stroke, fill, null);
-		Rule rule = styleFactory.createRule();
-		rule.symbolizers().add(sym);
-		FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle(new Rule[]{rule});
-		Style style = styleFactory.createStyle();
-		style.featureTypeStyles().add(fts);
-
-		//create and add layer
-		Layer layer = new FeatureLayer(shpFileNUTS.getFeatureCollection(f2), style);
-		map.addLayer(layer);
-
-		//
-		//JMapFrame.showMap(map);
-		saveImage(map, "H:/desktop/ex.png", 800);
-		 */
-	}
 
 
 
-
-
-	public static void saveImage(final MapContent map, final String file, final int imageWidth) {
-		try {
-			ReferencedEnvelope mapBounds = map.getViewport().getBounds();
-			Rectangle imageBounds = new Rectangle(0, 0, imageWidth, (int) Math.round(imageWidth * mapBounds.getSpan(1) / mapBounds.getSpan(0)));
-			BufferedImage image = new BufferedImage(imageBounds.width, imageBounds.height, BufferedImage.TYPE_INT_RGB);
-
-			Graphics2D gr = image.createGraphics();
-			gr.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			gr.setPaint(Color.WHITE);
-			gr.fill(imageBounds);
-
-			GTRenderer renderer = new StreamingRenderer();
-			renderer.setMapContent(map);
-			renderer.paint(gr, imageBounds, mapBounds);
-
-			ImageIO.write(image, "png", new File(file));
-		} catch (Exception e) { e.printStackTrace(); }
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	
+	
+	
+	
+	
 	private static void filterE4ValidationDataAggregatesNUTS2() {
 
 		//load validation data
---
+++ src/main/java/eu/ec/estat/geostat/mapping/NUTSMap.java
@@ -0,0 +1,133 @@
+/**
+ * 
+ */
+package eu.ec.estat.geostat.mapping;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.geotools.map.FeatureLayer;
+import org.geotools.map.Layer;
+import org.geotools.map.MapContent;
+import org.geotools.map.MapViewport;
+import org.geotools.referencing.CRS;
+import org.geotools.renderer.GTRenderer;
+import org.geotools.renderer.lite.StreamingRenderer;
+import org.geotools.styling.FeatureTypeStyle;
+import org.geotools.styling.Fill;
+import org.geotools.styling.PolygonSymbolizer;
+import org.geotools.styling.Rule;
+import org.geotools.styling.Stroke;
+import org.geotools.styling.Style;
+import org.geotools.styling.StyleFactory;
+import org.opengis.filter.FilterFactory;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import eu.ec.estat.geostat.io.ShapeFile;
+
+/**
+ * 
+ * Library to produce NUTS based maps.
+ * 
+ * @author Julien Gaffuri
+ *
+ */
+public class NUTSMap {
+	//https://github.com/geotools/geotools/blob/master/docs/src/main/java/org/geotools/tutorial/style/StyleLab.java
+	//http://docs.geotools.org/latest/userguide/library/render/gtrenderer.html
+	//http://docs.geotools.org/latest/userguide/library/render/index.html
+	//http://gis.stackexchange.com/questions/123903/how-to-create-a-map-and-save-it-to-an-image-with-geotools
+
+
+
+	private static CoordinateReferenceSystem LAEA_CRS = null;
+	static{
+		try { LAEA_CRS = CRS.decode("EPSG:3035"); } catch (Exception e) { e.printStackTrace(); }
+	}
+
+	//TODO change with resource ones
+	public static String NUTS_SHP_LVL2 = "H:/geodata/gisco_stat_units/NUTS_2013_01M_SH/data/NUTS_RG_01M_2013_LAEA_lvl2.shp";
+	public static String NUTS_SHP_LVL3 = "H:/geodata/gisco_stat_units/NUTS_2013_01M_SH/data/NUTS_RG_01M_2013_LAEA_lvl3.shp";
+
+	MapContent map = null;
+
+	public NUTSMap(){
+		this("NUTS map");
+	}
+
+	public NUTSMap(String title){
+		map = new MapContent();
+		map.setTitle(title);
+		map.getViewport().setCoordinateReferenceSystem(LAEA_CRS);
+		map.getViewport().setBounds(new ReferencedEnvelope(2500000.0,5600000.0,1700000.0,4700000.0, LAEA_CRS ));
+	}
+
+
+	public void produce() {
+
+		ShapeFile shpFileNUTS = new ShapeFile(NUTS_SHP_LVL2);
+
+		//style
+		// create a partially opaque outline stroke
+		StyleFactory styleFactory = CommonFactoryFinder.getStyleFactory();
+		FilterFactory filterFactory = CommonFactoryFinder.getFilterFactory();
+
+		Stroke stroke = styleFactory.createStroke(
+				filterFactory.literal(Color.WHITE),
+				filterFactory.literal(1),
+				filterFactory.literal(0.5));
+
+		// create a partial opaque fill
+		Fill fill = styleFactory.createFill(
+				filterFactory.literal(Color.gray),
+				filterFactory.literal(0.5));
+		PolygonSymbolizer sym = styleFactory.createPolygonSymbolizer(stroke, fill, null);
+		Rule rule = styleFactory.createRule();
+		rule.symbolizers().add(sym);
+		FeatureTypeStyle fts = styleFactory.createFeatureTypeStyle(new Rule[]{rule});
+		Style style = styleFactory.createStyle();
+		style.featureTypeStyles().add(fts);
+
+		//create and add layer
+		Layer layer = new FeatureLayer(shpFileNUTS.getFeatureCollection(), style);
+		map.addLayer(layer);
+
+		//
+		//JMapFrame.showMap(map);
+		saveImage(map, "H:/desktop/ex.png", 800);
+	}
+
+
+
+
+
+	public static void saveImage(final MapContent map, final String file, final int imageWidth) {
+		try {
+			ReferencedEnvelope mapBounds = map.getViewport().getBounds();
+			Rectangle imageBounds = new Rectangle(0, 0, imageWidth, (int) Math.round(imageWidth * mapBounds.getSpan(1) / mapBounds.getSpan(0)));
+			BufferedImage image = new BufferedImage(imageBounds.width, imageBounds.height, BufferedImage.TYPE_INT_RGB);
+
+			Graphics2D gr = image.createGraphics();
+			gr.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			gr.setPaint(Color.WHITE);
+			gr.fill(imageBounds);
+
+			GTRenderer renderer = new StreamingRenderer();
+			renderer.setMapContent(map);
+			renderer.paint(gr, imageBounds, mapBounds);
+
+			ImageIO.write(image, "png", new File(file));
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+
+
+
+}
