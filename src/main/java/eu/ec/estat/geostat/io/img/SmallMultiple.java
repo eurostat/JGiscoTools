3d62ccccba5dd84251a60d89a6bf41d635c7d80d
julien Gaffuri <julien.gaffuri@gmail.com>
2017-04-27 16:54:08 +0200

    a

---
+++ src/main/java/eu/ec/estat/geostat/io/img/SmallMultiple.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package eu.ec.estat.geostat.io.img;
+
+/**
+ * Library for the generation of small multiple
+ * 
+ * @author Julien Gaffuri
+ *
+ */
+public class SmallMultiple {
+
+	public static void make(String[] imgs, int rowNb, int colNb, String out) {
+		//it is assumed all images have the same dimension
+		//TODO
+
+	}
+
+}
