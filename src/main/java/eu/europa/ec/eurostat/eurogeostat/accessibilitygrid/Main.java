6aa45b97e5c7e3e7ace8c064e6b845ee968d0ac5
GAFFURI Julien (ESTAT) <Julien.GAFFURI@ec.europa.eu>
2019-09-06 18:26:46 +0200

    a

--- src/main/java/eu/europa/ec/eurostat/eurogeostat/accessibilitygrid/Main.java
+++ src/main/java/eu/europa/ec/eurostat/eurogeostat/accessibilitygrid/Main.java
@@ -3,6 +3,14 @@
  */
 package eu.europa.ec.eurostat.eurogeostat.accessibilitygrid;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.opencarto.datamodel.Feature;
+import org.opencarto.io.SHPUtil;
+import org.opencarto.util.JTSGeomUtil;
+import org.opencarto.util.ProjectionUtil;
+
 /**
  * @author julien Gaffuri
  *
@@ -11,13 +19,31 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		//example
 		//https://krankenhausatlas.statistikportal.de/
 
+		System.out.println("Start");
 
 		//create xkm grid
-		//ShapeFile.
+		String outPath = "C:/Users/gaffuju/Desktop";
 
 
+
+		double res = 5000;
+		int epsg = 3035;
+		Collection<Feature> fs = new ArrayList<Feature>();
+		for(double x=0; x<10000000; x+=res)
+			for(double y=0; y<10000000; y+=res) {
+				Feature f = new Feature();
+				f.setGeom( JTSGeomUtil.createPolygon( x,y, x+res,y, x+res,y+res, x,y+res, x,y ) );
+				f.id = "CRS"+Integer.toString((int)epsg)+"RES"+Integer.toString((int)res)+x+y;
+				f.set("cellId", f.id);
+				fs.add(f);
+			}
+		System.out.println("Save " + fs.size() + " cells");
+		SHPUtil.saveSHP(fs, outPath+"/out/grid.shp", ProjectionUtil.getCRS(epsg));
+
+		System.out.println("End");
 	}
 
 }
