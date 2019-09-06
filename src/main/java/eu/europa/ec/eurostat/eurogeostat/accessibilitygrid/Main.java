8d8e474676be790eccc0d4c57bd3d4256a691870
gaffuju <gaffuju@S-EST-TPOL.net1.cec.eu.int>
2019-09-06 14:14:55 +0200

    a

---
+++ src/main/java/eu/europa/ec/eurostat/eurogeostat/accessibilitygrid/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.eurogeostat.accessibilitygrid;
+
+/**
+ * @author julien Gaffuri
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//https://krankenhausatlas.statistikportal.de/
+
+		
+		
+	}
+
+}
