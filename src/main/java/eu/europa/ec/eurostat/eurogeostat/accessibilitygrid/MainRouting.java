d532a15818789a59b3bc6967f0fac0d8efe7f9e1
jgaffuri <jgaf@protonmail.com>
2019-10-07 10:43:43 +0200

    test astar

--- src/main/java/eu/europa/ec/eurostat/eurogeostat/accessibilitygrid/MainRouting.java
+++ src/main/java/eu/europa/ec/eurostat/eurogeostat/accessibilitygrid/MainRouting.java
@@ -11,11 +11,14 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.filter.text.cql2.CQL;
-import org.geotools.graph.path.DijkstraShortestPathFinder;
+import org.geotools.graph.path.AStarShortestPathFinder;
 import org.geotools.graph.path.Path;
-import org.geotools.graph.structure.Node;
 import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.index.strtree.ItemBoundable;
+import org.locationtech.jts.index.strtree.ItemDistance;
+import org.locationtech.jts.index.strtree.STRtree;
 import org.opencarto.datamodel.Feature;
 import org.opencarto.io.CSVUtil;
 import org.opencarto.io.SHPUtil;
@@ -50,7 +53,7 @@ public class MainRouting {
 		FeatureCollection<?,?> fc = SHPUtil.getSimpleFeatures(networkFile, CQL.toFilter("ICC = 'DE'"));
 
 		logger.info("Load grid data");
-		int resKM = 10;
+		int resKM = 50;
 		ArrayList<Feature> cells = SHPUtil.loadSHP(gridpath + resKM+"km/grid_"+resKM+"km.shp", CQL.toFilter("CNTR_ID = 'DE'")).fs;
 		logger.info(cells.size() + " cells");
 
@@ -64,20 +67,26 @@ public class MainRouting {
 		//- GST = GF0904: Tertiary education (ISCED-97 Level 5, 6): Universities
 		//- GST = GF0905: Education not definable by level
 
-		//build routing
+		logger.info("Build routing");
 		Routing rt = new Routing(fc);
 
 		//final data
-		Collection<HashMap<String, String>> data = new ArrayList<>();
+		Collection<HashMap<String, String>> cellData = new ArrayList<>();
 		Collection<Feature> routes = new ArrayList<>();
 
-		/*/build poi spatial index
+		//build poi spatial index, to quickly retrieve the X nearest (with euclidian distance) pois from cell center
 		STRtree poiIndex = new STRtree();
 		for(Feature poi : pois)
-			//TODO envelope of point? Use another type of index? KdTree
 			poiIndex.insert(poi.getDefaultGeometry().getEnvelopeInternal(), poi);
-
-		int nbNearest = 5;*/
+		int nbNearest = 5;
+		ItemDistance itemDist = new ItemDistance() {
+			@Override
+			public double distance(ItemBoundable item1, ItemBoundable item2) {
+				Feature f1 = (Feature) item1.getItem();
+				Feature f2 = (Feature) item2.getItem();
+				return f1.getDefaultGeometry().distance(f2.getDefaultGeometry());
+			}
+		};
 
 		//go through cells
 		for(Feature cell : cells) {
@@ -87,25 +96,41 @@ public class MainRouting {
 			//get cell centroid as origin point
 			Coordinate oC = cell.getDefaultGeometry().getCentroid().getCoordinate();
 			//TODO: get and build local routing only
-			DijkstraShortestPathFinder dpf = rt.getDijkstraShortestPathFinder(oC);
+			//TODO: try AStar
+			//logger.info("Build DijkstraShortestPathFinder");
+			//DijkstraShortestPathFinder pf = rt.getDijkstraShortestPathFinder(oC);
 
-			//get X nearest pois with straight line
-			//pois_ = poiIndex.nearestNeighbour(cell.getDefaultGeometry().getEnvelopeInternal(), cell, itemDist, nbNearest);
+			logger.info("Get " + nbNearest + " nearest pois");
+			Envelope env = cell.getDefaultGeometry().getEnvelopeInternal(); env.expandBy(1000);
+			Object[] pois_ = poiIndex.nearestNeighbour(env, cell, itemDist, nbNearest);
 
-			//compute the routes to all pois. TODO select only the ones nearby using spatial index
-			//get the shortest/fastest
+			//compute the routes to all pois to get the shortest/fastest
+			logger.info("Compute routes to pois. Nb="+pois_.length);
 			Path pMax = null;
-			for(Object poi_ : pois) {
+			for(Object poi_ : pois_) {
 				Feature poi = (Feature) poi_;
 				Coordinate dC = poi.getDefaultGeometry().getCentroid().getCoordinate();
-				Node dN = rt.getNode(dC);
-				Path p = dpf.getPath(dN );
+				AStarShortestPathFinder pf = rt.getAStarShortestPathFinder(oC, dC);
+				pf.calculate();
+				Path p = null;
+				try { p = pf.getPath();
+				} catch (Exception e) {
+					logger.warn("Could not compute path. " + e.getMessage());
+					continue;
+				}
+				//Path p = pf.getPath( rt.getNode(dC) );
+				if(p==null) continue;
 				//get shortest/fastest
 				if(pMax==null) { pMax=p; continue; }
 				//TODO get "value" of p and compare with those of pMax
+				//routes.add( Routing.toFeature(p) );
 			}
+			if(pMax==null) {
+				logger.warn("Could not handle grid cell " + cellId);
+				continue;
+			}
+			//store data at grid cell level
 			//TODO
-			//store data
 			//store route
 			Feature f = Routing.toFeature(pMax);
 			//TODO f.setAttribute(key, value);
@@ -113,9 +138,9 @@ public class MainRouting {
 		}
 
 		logger.info("Save data");
-		CSVUtil.save(data, path + "data_DE_10km.csv");
-		logger.info("Save routes");
-		SHPUtil.saveSHP(routes, path + "routes_DE_10km.shp", crs);
+		CSVUtil.save(cellData, path + "cell_data_DE_"+resKM+"km.csv");
+		logger.info("Save routes. Nb="+routes.size());
+		SHPUtil.saveSHP(routes, path + "routes_DE_"+resKM+"km.shp", crs);
 
 		logger.info("End");
 	}
