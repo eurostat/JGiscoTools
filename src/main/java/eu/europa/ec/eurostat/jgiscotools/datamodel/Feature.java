b311380b03de0cc10922cbbadd2544fc3c14d49d
jgaffuri <jgaf@protonmail.com>
2019-10-24 20:21:00 +0200

    feature comment

--- src/main/java/eu/europa/ec/eurostat/jgiscotools/datamodel/Feature.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/datamodel/Feature.java
@@ -9,33 +9,58 @@ import java.util.Map;
 import org.locationtech.jts.geom.Geometry;
 
 /**
+ * 
+ * A POJO structure to represent simple geographical feature. It makes
+ * manipulation of these features even simpler than with SimpleFeature.
+ * 
  * @author julien Gaffuri
  *
  */
-public class Feature /*implements SimpleFeature*/ {
+public class Feature {
 
-	//id
-	private static int ID;
+	/** The feature identifier */
 	private String id;
-	public String getID() { return id; }
-	public void setID(String id) { this.id = id; }
 
-	public Feature(){
+	public String getID() {
+		return id;
+	}
+
+	public void setID(String id) {
+		this.id = id;
+	}
+
+	public Feature() {
 		id = String.valueOf(ID++);
 	}
 
-	//geometry
-	private Geometry geometry;
-	public Geometry getDefaultGeometry(){ return geometry; }
-	public void setDefaultGeometry(Geometry geometry){ this.geometry = geometry; }
+	private static int ID;
 
-	//attributes
+	/** The feature Geometry */
+	private Geometry geometry;
+
+	public Geometry getDefaultGeometry() {
+		return geometry;
+	}
+
+	public void setDefaultGeometry(Geometry geometry) {
+		this.geometry = geometry;
+	}
+
+	/** The feature attributes, as a dictionary <key,value> */
 	private Map<String, Object> atts;
-	public Map<String, Object> getAttributes(){
-		if(atts==null) atts = new HashMap<String, Object>();
+
+	public Map<String, Object> getAttributes() {
+		if (atts == null)
+			atts = new HashMap<String, Object>();
 		return atts;
 	}
-	public Object getAttribute(String att) { return getAttributes().get(att); }
-	public Object setAttribute(String key, Object value) { return getAttributes().put(key, value); }
+
+	public Object getAttribute(String att) {
+		return getAttributes().get(att);
+	}
+
+	public Object setAttribute(String key, Object value) {
+		return getAttributes().put(key, value);
+	}
 
 }
