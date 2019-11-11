d1082ed65db7ed07d1ad4fc3b2eaf16a433c37af
jgaffuri <jgaf@protonmail.com>
2019-11-11 15:54:36 +0100

    test grid tiling

--- src/main/java/eu/europa/ec/eurostat/jgiscotools/zzz/PopGridTiling.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/zzz/PopGridTiling.java
@@ -1,8 +1,31 @@
 package eu.europa.ec.eurostat.jgiscotools.zzz;
 
+import org.apache.log4j.Logger;
+
+import eu.europa.ec.eurostat.java4eurostat.base.StatsHypercube;
+import eu.europa.ec.eurostat.java4eurostat.io.CSV;
+import eu.europa.ec.eurostat.jgiscotools.gridstat.tiling.GridStatTiler;
+
 public class PopGridTiling {
+	private static Logger logger = Logger.getLogger(PopGridTiling.class.getName());
 
 	public static void main(String[] args) {
+		logger.info("Start");
+
+		String basePath = "E:/gridstat/data/";
+
+		logger.info("Load data");
+		StatsHypercube sh = CSV.load(basePath+"pop_grid/pop_grid_2006_1km.csv", "TOT_P");
+		logger.info(sh.stats.size());
+
+		logger.info("Build tiles");
+		GridStatTiler gst = new GridStatTiler(sh);
+		gst.createTiles(-1, 1);
+
+		logger.info("Save tiles");
+		gst.save(basePath+"pop_grid/pop_grid_2006_1km_tiled/");
+
+		logger.info("End");
 	}
 
 }
