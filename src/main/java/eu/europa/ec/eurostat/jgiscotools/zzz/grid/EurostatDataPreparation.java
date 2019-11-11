5aceaf490e4f241b6c41dceea7c7f30fc4b258bc
jgaffuri <jgaf@protonmail.com>
2019-11-11 11:58:43 +0100

    import of grid production procedures

---
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/zzz/grid/EurostatDataPreparation.java
@@ -0,0 +1,129 @@
+package eu.europa.ec.eurostat.jgiscotools.zzz.grid;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.apache.log4j.Logger;
+import org.geotools.referencing.CRS;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Polygon;
+import org.locationtech.jts.operation.buffer.BufferParameters;
+import org.locationtech.jts.operation.union.CascadedPolygonUnion;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import eu.europa.ec.eurostat.jgiscotools.CountriesUtil;
+import eu.europa.ec.eurostat.jgiscotools.algo.base.Union;
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+import eu.europa.ec.eurostat.jgiscotools.io.SHPUtil;
+
+public class EurostatDataPreparation {
+	static Logger logger = Logger.getLogger(EurostatDataPreparation.class.getName());
+
+	//use: -Xms2G -Xmx6G
+	public static void main(String[] args) throws Exception {
+		logger.info("Start");
+
+		String path = "E:/gridstat/data/CNTR_100k/";
+
+		logger.info("Produce country geometry as the union of different versions");
+		produceCountriesUnionVersions(path);
+
+		logger.info("Produce Europe 100k as union of countries");
+		SHPUtil.union(path+"CNTR_RG_100K_union_LAEA.shp", path+"Europe_100K_union_LAEA.shp", 0);
+		//NB: try iterative union or buffer(0) directly
+
+		//buffering
+		int bufferDistance = 1500;
+		logger.info("Produce buffers (" + bufferDistance + ") of countries");
+		buffer(path+"CNTR_RG_100K_union_LAEA.shp", path+"CNTR_RG_100K_union_buff_" + bufferDistance + "_LAEA.shp", bufferDistance, 4, BufferParameters.CAP_ROUND);
+		logger.info("Produce Europe (" + bufferDistance + ") buffer");
+		buffer(path+"Europe_100K_union_LAEA.shp", path+"Europe_100K_union_buff_" + bufferDistance + "_LAEA.shp", bufferDistance, 4, BufferParameters.CAP_ROUND);
+
+		//TODO remove country holes ?
+
+		logger.info("End");
+	}
+
+
+
+	//compute the buffers of features of a SHP file
+	public static void buffer(String inFile, String outFile, double bufferDistance, int quadrantSegments, int endCapStyle){
+
+		//load data
+		ArrayList<Feature> fs = SHPUtil.loadSHP(inFile).fs;
+
+		for(Feature f : fs) {
+			logger.info(f.getAttribute("CNTR_ID"));
+			Geometry geom = (Geometry) f.getDefaultGeometry();
+			Collection<Geometry> geoms = getGeometries(geom);
+			logger.info(geoms.size() + " components");
+			logger.info("Compute buffers");
+			Collection<Geometry> buffs = new ArrayList<>();
+			for(Geometry g : geoms)
+				buffs.add(g.buffer(bufferDistance, quadrantSegments, endCapStyle));
+			logger.info("Compute union of buffers");
+			Geometry buff = new CascadedPolygonUnion(buffs ).union();
+			if(buff instanceof Polygon) buff = buff.getFactory().createMultiPolygon(new Polygon[] {(Polygon)buff});
+			f.setDefaultGeometry(buff);
+		}
+
+		logger.info("Save");
+		SHPUtil.saveSHP(fs, outFile, SHPUtil.getCRS(inFile));
+	}
+
+	//return list of geometries that are not GeometryCollection
+	public static Collection<Geometry> getGeometries(Geometry geom){
+		Collection<Geometry> out = new HashSet<Geometry>();
+		int nb = geom.getNumGeometries();
+		if(nb == 0)
+			return out;
+		if(nb == 1)
+			out.add(geom.getGeometryN(0));
+		else
+			for(int i=0; i<nb; i++)
+				out.addAll( getGeometries(geom.getGeometryN(i)) );
+		return out;
+	}
+
+
+
+	//produce country geometry as the union of different versions
+	private static void produceCountriesUnionVersions(String path) throws Exception {
+		CoordinateReferenceSystem crs = CRS.decode("EPSG:3035");
+
+		Collection<Feature> cnts = new ArrayList<>();
+		for(String cntC : CountriesUtil.EuropeanCountryCodes) {
+			logger.info(cntC);
+
+			Collection<Geometry> polys = new ArrayList<Geometry>();
+			for(String version : new String[] {"2004","2010","2013","2016"}) {
+				logger.info(version);
+
+				Geometry cntGeomV;
+				try {
+					cntGeomV = CountriesUtil.getEuropeanCountry(cntC, path+"CNTR_RG_100K_"+version+"_LAEA.shp").getDefaultGeometry();
+				} catch (Exception e) {
+					logger.warn("Not found");
+					continue;
+				}
+				cntGeomV = cntGeomV.buffer(0);
+				polys.add(cntGeomV);
+				//for(Geometry poly : getGeometries(cntGeomV))
+				//	polys.add( poly.buffer(0) );
+			}
+
+			logger.info("Compute union");
+			Geometry cntGeom = Union.polygonsUnionAll(polys);
+
+			Feature cnt = new Feature();
+			cnt.setAttribute("CNTR_ID", cntC);
+			if(cntGeom instanceof Polygon) cntGeom = cntGeom.getFactory().createMultiPolygon(new Polygon[] {(Polygon)cntGeom});
+			cnt.setDefaultGeometry(cntGeom);
+			cnts.add(cnt);
+		}
+		logger.info("Save");
+		SHPUtil.saveSHP(cnts, path+"CNTR_RG_100K_union_LAEA.shp", crs);
+	}
+
+}
---
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/zzz/grid/EurostatGridsProduction.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.jgiscotools.zzz.grid;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+import eu.europa.ec.eurostat.jgiscotools.feature.FeatureUtil;
+import eu.europa.ec.eurostat.jgiscotools.grid.Grid;
+import eu.europa.ec.eurostat.jgiscotools.grid.GridUtil;
+import eu.europa.ec.eurostat.jgiscotools.io.GeoPackageUtil;
+import eu.europa.ec.eurostat.jgiscotools.io.SHPUtil;
+
+/**
+ * Examples to produce European grids based on ETRS89-LAEA coordinate reference system (EPSG:3035)
+ * for various resolutions. The cells are tagged by country and identified with their standard code.
+ * 
+ * @author julien Gaffuri
+ *
+ */
+public class EurostatGridsProduction {
+	static Logger logger = Logger.getLogger(EurostatGridsProduction.class.getName());
+
+	//see also:
+	//https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2
+	//https://www.efgs.info/data/
+	//https://esdac.jrc.ec.europa.eu/content/european-reference-grids
+
+	//the different resolutions, in KM
+	static int[] resKMs = new int[] {100,50,20,10,5,2,1};
+
+	public static void main(String[] args) throws Exception {
+		logger.info("Start");
+
+		logger.setLevel(Level.ALL);
+		Grid.logger.setLevel(Level.ALL);
+
+		String outpath = "E:/gridstat/data/out/";
+		String path = "E:/gridstat/data/CNTR_100k/";
+		CoordinateReferenceSystem crs = CRS.decode("EPSG:3035");
+		int bufferDistance = 1500;
+
+		logger.info("Get European countries (buffer) ...");
+		ArrayList<Feature> cntsBuff = SHPUtil.loadSHP(path+"CNTR_RG_100K_union_buff_"+bufferDistance+"_LAEA.shp").fs;
+
+		/*
+		logger.info("Get land area...");
+		Collection<Geometry> landGeometries = FeatureUtil.getGeometriesSimple( SHPUtil.loadSHP(path+"CNTR_RG_100K_union_LAEA.shp").fs );
+		//TODO tile land areas
+		SpatialIndex landGeometriesIndex = new STRtree();
+		for(Geometry g : landGeometries) landGeometriesIndex.insert(g.getEnvelopeInternal(), g);
+		landGeometries = null;
+		 */
+
+		//build pan-European grids
+		for(int resKM : resKMs) {
+			logger.info("Make " + resKM + "km grid...");
+
+			//build grid
+			Grid grid = new Grid()
+					.setResolution(resKM*1000)
+					.setEPSGCode("3035")
+					.addGeometryToCover( FeatureUtil.getGeometries(cntsBuff) )
+					;
+			Collection<Feature> cells = grid.getCells();
+
+			logger.info("Assign country codes...");
+			GridUtil.assignRegionCode(cells, "CNTR_ID", cntsBuff, 0, "CNTR_ID");
+
+			logger.info("Filtering " + cells.size() + " cells...");
+			GridUtil.filterCellsWithoutRegion(cells, "CNTR_ID");
+			logger.info(cells.size() + " cells left");
+
+			//TODO assign also nuts code? for each level?
+
+			//TODO too slow - tile that
+			//logger.info("Assign land proportion...");
+			//GridUtil.assignLandProportion(cells, "LAND_PC", landGeometriesIndex, 2);
+
+			//save as GPKG
+			logger.info("Save " + cells.size() + " cells as GPKG...");
+			GeoPackageUtil.save(cells, outpath+"grid_"+resKM+"km.gpkg", crs);
+
+			//save as SHP
+			if(resKM>3) {
+				logger.info("Save " + cells.size() + " cells as SHP...");
+				SHPUtil.saveSHP(cells, outpath + "grid_"+resKM+"km_shp" + "/grid_"+resKM+"km.shp", crs);
+			}
+		}
+
+
+
+		/*
+		//build country 1km grids by country
+		for(String countryCode : CountriesUtil.EuropeanCountryCodes) {
+
+			logger.info("Make 1km grid for " + countryCode + "...");
+
+			//get country geometry (buffer)
+			Geometry countryGeom = SHPUtil.loadSHP(path+"CNTR_RG_100K_union_buff_"+bufferDistance+"_LAEA.shp", CQL.toFilter("CNTR_ID = '"+countryCode+"'"))
+					.fs.iterator().next().getDefaultGeometry();
+
+			//build cells
+			StatGrid grid = new StatGrid()
+					.setResolution(1000)
+					.setEPSGCode("3035")
+					.setGeometryToCover(countryGeom)
+					;
+			Collection<Feature> cells = grid.getCells();
+
+			//set country code to cells
+			for(Feature cell : cells) cell.setAttribute("CNTR_ID", countryCode);
+
+			logger.info("Save " + cells.size() + " cells as SHP...");
+			SHPUtil.saveSHP(cells, outpath+"grid_1km_shp/grid_1km_"+countryCode+".shp", crs);
+			//logger.info("Save " + cells.size() + " cells as GPKG...");
+			//GeoPackageUtil.save(cells, outpath+"1km/grid_1km_"+countryCode+".gpkg", crs);
+		}
+		 */
+
+		logger.info("End");
+	}
+
+}
