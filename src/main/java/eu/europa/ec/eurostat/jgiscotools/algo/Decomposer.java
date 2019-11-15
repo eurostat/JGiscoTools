6c8c6877a0698af69623b46d564abf3e0dbb9a43
jgaffuri <jgaf@protonmail.com>
2019-11-15 17:45:02 +0100

    land area decomposition

---
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/algo/Decomposer.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.jgiscotools.algo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+import org.locationtech.jts.geom.Geometry;
+
+import eu.europa.ec.eurostat.jgiscotools.algo.Partition.GeomType;
+import eu.europa.ec.eurostat.jgiscotools.algo.Partition.PartitionedOperation;
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+import eu.europa.ec.eurostat.jgiscotools.feature.FeatureUtil;
+
+/**
+ * 
+ * Decompose a set of geometries into a lot of smaller geometries, based on a quadtree decomposition.
+ * 
+ * @author julien Gaffuri
+ *
+ */
+public class Decomposer {
+	public static Logger logger = Logger.getLogger(Decomposer.class.getName());
+
+
+	/**
+	 * @param fs the input features
+	 * @param maxCoordinatesNumber
+	 * @param objMaxCoordinateNumber
+	 * @param gt
+	 * @param midRandom
+	 * @return
+	 */
+	public static Collection<Geometry> decompose(Collection<Feature> fs, int maxCoordinatesNumber, int objMaxCoordinateNumber, GeomType gt, double midRandom) {
+		final Collection<Geometry> out = new ArrayList<>();
+		PartitionedOperation op = new PartitionedOperation() {
+			@Override
+			public void run(Partition p) {
+				logger.debug(p.getCode());
+				out.addAll(FeatureUtil.getGeometriesSimple(p.getFeatures()));
+			}
+		};
+		Partition.runRecursively(fs, op, maxCoordinatesNumber, objMaxCoordinateNumber, true, gt, midRandom);
+		return out;
+	}
+
+}
--- src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/grid/geomprod/DecomposeLand.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/grid/geomprod/DecomposeLand.java
@@ -3,49 +3,39 @@
  */
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes.grid.geomprod;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.geotools.filter.text.cql2.CQL;
 import org.geotools.referencing.CRS;
 import org.locationtech.jts.geom.Geometry;
 
-import eu.europa.ec.eurostat.jgiscotools.algo.Partition;
+import eu.europa.ec.eurostat.jgiscotools.algo.Decomposer;
 import eu.europa.ec.eurostat.jgiscotools.algo.Partition.GeomType;
-import eu.europa.ec.eurostat.jgiscotools.algo.Partition.PartitionedOperation;
 import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
-import eu.europa.ec.eurostat.jgiscotools.feature.FeatureUtil;
 import eu.europa.ec.eurostat.jgiscotools.io.GeoPackageUtil;
 
 /**
  * @author julien Gaffuri
  *
  */
-public class Decompose {
-	static Logger logger = Logger.getLogger(Decompose.class.getName());
+public class DecomposeLand {
+	static Logger logger = Logger.getLogger(DecomposeLand.class.getName());
 
 	public static void main(String[] args) throws Throwable {
 		logger.info("Start");
+		Decomposer.logger.setLevel(Level.ALL);
 
 		String path = "E:/workspace/gridstat/data/CNTR_100k/";
 
 		logger.info("Load data...");
-		Collection<Feature> fs = GeoPackageUtil.getFeatures(path+"CNTR_RG_100K_union_LAEA.gpkg", CQL.toFilter("CNTR_ID='BE'"));
-		System.out.println(fs.size());
-
+		Collection<Feature> fs = GeoPackageUtil.getFeatures(path+"CNTR_RG_100K_union_LAEA.gpkg"/*, CQL.toFilter("CNTR_ID='FR'")*/);
+		//Collection<Feature> fs = GeoPackageUtil.getFeatures(path+"Europe_100K_union_LAEA.gpkg"/*, CQL.toFilter("CNTR_ID='FR'")*/);
+		logger.info(fs.size());
 
 		logger.info("Run decomposition...");
-		final Collection<Geometry> landGeometries = new ArrayList<>();
-		PartitionedOperation op = new PartitionedOperation() {
-			@Override
-			public void run(Partition p) {
-				System.out.println(p.getCode());
-				landGeometries.addAll(FeatureUtil.getGeometriesSimple(p.getFeatures()));
-			}
-		};
-		Partition.runRecursively(fs, op, 100000, 10000, true, GeomType.ONLY_AREAS, 0);
-		System.out.println(landGeometries.size());
+		Collection<Geometry> landGeometries = Decomposer.decompose(fs, 1000, 1000, GeomType.ONLY_AREAS, 0);
+		logger.info(landGeometries.size());
 
 		logger.info("Save...");
 		GeoPackageUtil.saveGeoms(landGeometries, path + "land_areas.gpkg", CRS.decode("EPSG:3035"));
