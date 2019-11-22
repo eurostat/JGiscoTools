c2c4655848ecf53244846747f26eaf86e0220a61
jgaffuri <jgaf@protonmail.com>
2019-11-22 10:12:50 +0100

    shp merge

--- src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/TestGDBRead.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/TestGDBRead.java
@@ -3,6 +3,21 @@
  */
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes;
 
+import java.util.Collection;
+
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.feature.DefaultFeatureCollection;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.referencing.CRS;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+import eu.europa.ec.eurostat.jgiscotools.feature.SimpleFeatureUtil;
+import eu.europa.ec.eurostat.jgiscotools.io.GeoPackageUtil;
+import eu.europa.ec.eurostat.jgiscotools.io.SHPUtil;
+import eu.europa.ec.eurostat.jgiscotools.io.ShapeFile;
+
 /**
  * @author gaffuju
  *
@@ -12,25 +27,36 @@ public class TestGDBRead {
 	public static void main(String[] args) throws Exception {
 		//See https://docs.geotools.org/stable/userguide/library/data/ogr.html
 		//See https://gis.stackexchange.com/questions/243746/using-geotools-to-open-esri-file-geodatabase-filegdb
-		
+
 		System.out.println("start");
 
 
-		/*/merge ERM roadL
-		
+		//merge ERM roadL
+
 		String egpath = "E:/dissemination/shared-data/";
 
-		System.out.println("load");
-		Collection<Feature> networkSections = SHPUtil.loadSHP(egpath+"ERM/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_14_15_16.shp").fs;
-		System.out.println("load");
-		networkSections.addAll( SHPUtil.loadSHP(egpath+"ERM/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_984.shp").fs );
-		System.out.println("load");
-		networkSections.addAll( SHPUtil.loadSHP(egpath+"ERM/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_0.shp").fs );
+		SimpleFeatureType ft = SHPUtil.getSchema(egpath+"ERM/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_14_15_16.shp");
+		System.out.println(ft);
+		DefaultFeatureCollection sfc = new DefaultFeatureCollection(null, ft);
+		SimpleFeatureCollection f;
+
+		System.out.println("load 1");
+		f = SHPUtil.getSimpleFeatures(egpath+"ERM/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_14_15_16.shp", null);
+		sfc.addAll(f); f = null;
+		System.out.println(sfc.size());
+
+		System.out.println("load 2");
+		f = SHPUtil.getSimpleFeatures(egpath+"ERM/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_984.shp", null);
+		sfc.addAll(f); f = null;
+		System.out.println(sfc.size());
+
+		System.out.println("load 3");
+		f = SHPUtil.getSimpleFeatures(egpath+"ERM/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_0.shp", null);
+		sfc.addAll(f); f = null;
+		System.out.println(sfc.size());
 
 		System.out.println("save");
-		CoordinateReferenceSystem crs = CRS.decode("EPSG:3035");
-		GeoPackageUtil.save(networkSections, "E:/workspace/gridstat/data/RoadL.gpkg", crs, true);
-		*/
+		GeoPackageUtil.save(sfc, "E:/workspace/gridstat/data/RoadL.gpkg", true);
 
 		System.out.println("end");
 	}
