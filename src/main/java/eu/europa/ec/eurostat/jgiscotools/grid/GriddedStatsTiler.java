23a1b882d05fe3f46d4c93f279b02d71388a46c4
jgaffuri <jgaf@protonmail.com>
2019-12-07 10:01:12 +0100

    remove todos

--- src/main/java/eu/europa/ec/eurostat/jgiscotools/grid/GriddedStatsTiler.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/grid/GriddedStatsTiler.java
@@ -190,12 +190,8 @@ public class GriddedStatsTiler {
 				sht.stats.add(s_);
 			}
 
-			//TODO empty tiles: make empty file
-
 			//save as csv file
-			//TODO be sure order is x,y,val
 			//TODO handle case of more columns, when using multidimensional stats
-			//TODO add json with service information
 			CSV.save(sht, "val", folderPath + "/" +t.x+ "/" +t.y+ ".csv", ",", new Comparator<String>() {
 				@Override
 				public int compare(String s1, String s2) {
