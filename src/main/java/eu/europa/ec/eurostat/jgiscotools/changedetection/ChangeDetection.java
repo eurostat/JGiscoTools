69e703eab9dfc8204bbcd63af1177a81118237e4
jgaffuri <jgaf@protonmail.com>
2020-01-16 17:50:05 +0100

    a

--- src/main/java/eu/europa/ec/eurostat/jgiscotools/changedetection/ChangeDetection.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/changedetection/ChangeDetection.java
@@ -33,6 +33,8 @@ import eu.europa.ec.eurostat.jgiscotools.util.JTSGeomUtil;
 public class ChangeDetection {
 	private final static Logger LOGGER = LogManager.getLogger(ChangeDetection.class.getName());
 
+	//TODO: test datasets of points and lines
+
 	/**
 	 * The dataset in its initial version.
 	 */
