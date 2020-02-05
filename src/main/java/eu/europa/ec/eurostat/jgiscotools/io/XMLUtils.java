bf8521ae7fc02b82a7eb44fedab5e189121b62c5
jgaffuri <jgaf@protonmail.com>
2020-02-05 08:53:33 +0100

    XML IO

--- src/main/java/eu/europa/ec/eurostat/jgiscotools/io/XMLUtils.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/io/XMLUtils.java
@@ -3,7 +3,6 @@ package eu.europa.ec.eurostat.jgiscotools.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,12 +13,21 @@ import org.w3c.dom.Document;
  * @author julien Gaffuri
  *
  */
-public class XMLUtil {
+public class XMLUtils {
 
+	/**
+	 * Get a XML document from a URL.
+	 * 
+	 * @param urlString
+	 * @return
+	 */
 	public static Document parseXMLfromURL(String urlString){
 		try{
-			InputStream in = new URL(urlString).openConnection().getInputStream();
-			return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
+			Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new URL(urlString).openConnection().getInputStream());
+			new URL(urlString).openConnection().getInputStream().close();
+			return doc;
+			//InputStream in = new URL(urlString).openConnection().getInputStream();
+			//return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(in);
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -27,7 +35,12 @@ public class XMLUtil {
 		return null;
 	}
 
-
+	/**
+	 * Return the namespace of a XML document.
+	 * 
+	 * @param file
+	 * @return
+	 */
 	public static String getFileNameSpace(File file) {
 		String ns = null;
 		try {
@@ -40,6 +53,5 @@ public class XMLUtil {
 		}
 		return ns;
 	}
-
 
 }
