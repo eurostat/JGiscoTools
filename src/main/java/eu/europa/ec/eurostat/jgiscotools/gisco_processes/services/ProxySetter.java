2f2fb26ea2805ebc6753ac2d78ba27ff8142ddb3
jgaffuri <jgaf@protonmail.com>
2020-02-25 09:48:54 +0100

    handle other parameters

--- src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/services/ProxySetter.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/services/ProxySetter.java
@@ -1,49 +1,62 @@
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes.services;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.Authenticator;
 import java.net.PasswordAuthentication;
 import java.util.Scanner;
 
+/**
+ * Manage parameters from "user.home\eclipse.config" file.
+ * 
+ * @author gaffuju
+ *
+ */
 public class ProxySetter {
 
+
+	/**
+	 * Return a parameter name.
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static String get(String key) {
+		try {
+			Scanner scanner = new Scanner(new File(System.getProperty("user.home")+"\\eclipse.config"));
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				if("".equals(line))
+					continue;
+				String[] parts = line.split("=");
+
+				if(parts[0].equals(key)) {
+					scanner.close();
+					return parts[1];
+				}
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) { e.printStackTrace(); }
+		return null;
+	}
+
+
+	/**
+	 * 
+	 */
 	public static void loadProxySettings() {
 		System.setProperty("http.proxyHost", "pslux.ec.europa.eu");
 		System.setProperty("http.proxyPort", "8012");
 		System.setProperty("https.proxyHost", "pslux.ec.europa.eu");
 		System.setProperty("https.proxyPort", "8012");
 
-		try {
-			String proxy_username = null, proxy_pwd = null;
-			
-			Scanner scanner = new Scanner(new File(System.getProperty("user.home")+"\\eclipse.config"));
-			while (scanner.hasNextLine()) {
-			   String line = scanner.nextLine();
-				if("".equals(line)) {
-					continue;
-				}
-				String[] parts = line.split("=");
-				
-				if(parts[0].equals("proxy_username")) proxy_username = parts[1];
-				if(parts[0].equals("proxy_pwd")) proxy_pwd = parts[1];
-			}
-			scanner.close();
-			authenticate(proxy_username, proxy_pwd);
-			
-		} catch (Exception e) {
-			System.err.println("Could not load proxy settings");
-			e.printStackTrace();
-		}
-
-	}
-
-	private static void authenticate(String proxy_username, String proxy_pwd) {
 		Authenticator.setDefault(new Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(proxy_username, proxy_pwd.toCharArray());
+				return new PasswordAuthentication(get("proxy_username"), get("proxy_pwd").toCharArray());
 			}
 		});
+
 	}
 
 }
