e967b1087a521913fcbf14aa4bd579796af496f2
jgaffuri <jgaf@protonmail.com>
2020-03-06 21:58:58 +0100

    a

--- src/main/java/eu/europa/ec/eurostat/jgiscotools/io/GeoData.java
+++ src/main/java/eu/europa/ec/eurostat/jgiscotools/io/GeoData.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
@@ -19,6 +21,81 @@ import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
  *
  */
 public class GeoData {
+	private final static Logger LOGGER = LogManager.getLogger(GeoData.class);
+
+	private String filePath;
+	private String format;
+
+	public GeoData(String filePath) {
+		this.filePath = filePath;
+		this.format = FilenameUtils.getExtension(filePath).toLowerCase();
+		//TODO warnings
+	}
+
+	ArrayList<Feature> features = null;
+	public ArrayList<Feature> getFeatures() {
+		if(features == null) {
+			switch(format) {
+			case "shp":
+				features = SHPUtil.getFeatures(filePath);
+				break;
+			case "geojson":
+				features = GeoJSONUtil.getFeatures(filePath);
+				break;
+			case "gpkg":
+				features = GeoPackageUtil.getFeatures(filePath);
+				break;
+			default:
+				LOGGER.error("Could not retrieve features from data source: " + filePath);
+			}
+		}
+		return features;
+	}
+
+	SimpleFeatureType schema = null;
+	public SimpleFeatureType getSchema() {
+		if(schema == null) {
+			switch(format) {
+			case "shp":
+				schema = SHPUtil.getSchema(filePath);
+				break;
+			case "geojson":
+				schema = GeoJSONUtil.getSchema(filePath);
+				break;
+			case "gpkg":
+				schema = GeoPackageUtil.getSchema(filePath);
+				break;
+			default:
+				LOGGER.error("Could not retrieve schema from data source: " + filePath);
+			}
+		}
+		return schema;
+	}
+
+	public CoordinateReferenceSystem getCRS() {
+		return getSchema().getCoordinateReferenceSystem();
+	}
+
+	public void save() {
+		switch(format) {
+		case "shp":
+			SHPUtil.save(getFeatures(), filePath, getCRS());
+			break;
+		case "geojson":
+			GeoJSONUtil.save(getFeatures(), filePath, getCRS());
+			break;
+		case "gpkg":
+			GeoPackageUtil.save(getFeatures(), filePath, getCRS());
+			break;
+		default:
+			LOGGER.error("Unsuported output format: " + format);
+		}
+	}
+
+
+
+
+
 
 	/**
 	 * Get features
@@ -28,31 +105,11 @@ public class GeoData {
 	 * @throws Exception
 	 */
 	public static ArrayList<Feature> getFeatures(String filePath) throws Exception {
-		String input = FilenameUtils.getExtension(filePath).toLowerCase();
-		switch(input) {
-		case "shp":
-			return SHPUtil.getFeatures(filePath);
-		case "geojson":
-			return GeoJSONUtil.getFeatures(filePath);
-		case "gpkg":
-			return GeoPackageUtil.getFeatures(filePath);
-		default:
-			throw new Exception("Could not retrieve features from data source: "+filePath);
-		}
+		return new GeoData(filePath).getFeatures();
 	}
 
 	public static SimpleFeatureType getSchema(String filePath) throws Exception {
-		String format = FilenameUtils.getExtension(filePath).toLowerCase();
-		switch(format) {
-		case "shp":
-			return SHPUtil.getSchema(filePath);
-		case "geojson":
-			return GeoJSONUtil.getSchema(filePath);
-		case "gpkg":
-			return GeoPackageUtil.getSchema(filePath);
-		default:
-			throw new Exception("Could not retrieve schema from data source: "+filePath);
-		}
+		return new GeoData(filePath).getSchema();
 	}
 
 	public static CoordinateReferenceSystem getCRS(String filePath) throws Exception {
