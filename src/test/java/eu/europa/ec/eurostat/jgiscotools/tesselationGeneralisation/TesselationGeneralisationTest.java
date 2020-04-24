c6e6330df90e9aa8e5a248bf6936ec8bf4285854
jgaffuri <jgaf@protonmail.com>
2020-04-24 11:37:39 +0200

    tessgen test

---
+++ src/test/java/eu/europa/ec/eurostat/jgiscotools/tesselationGeneralisation/TesselationGeneralisationTest.java
@@ -0,0 +1,35 @@
+package eu.europa.ec.eurostat.jgiscotools.tesselationGeneralisation;
+
+import java.util.Collection;
+
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+import eu.europa.ec.eurostat.jgiscotools.io.GeoData;
+import eu.europa.ec.eurostat.jgiscotools.util.ProjectionUtil.CRSType;
+import junit.framework.TestCase;
+
+public class TesselationGeneralisationTest extends TestCase {
+
+	public static void main(String[] args) throws Exception {
+		junit.textui.TestRunner.run(TesselationGeneralisationTest.class);
+	}
+
+	public void test() {
+		for(int scaleM : new int[] {5, 10})
+			for(String cntr : new String[] {/*"bangladesh", "chile", "china_mainland", "indonesia",*/ "panama", "philippines"}) {
+				System.out.println(" *** " + cntr + " - scaleM: " + scaleM);
+
+				String inFile = "src/test/resources/tessgene/cntr/" + cntr + ".shp";
+				Collection<Feature> units = GeoData.getFeatures(inFile);
+
+				CRSType crsType = GeoData.getCRSType(inFile);
+				int roundNb = 10;
+				int maxCoordinatesNumber = 1000000;
+				int objMaxCoordinateNumber = 1000;
+				units = TesselationGeneralisation.runGeneralisation(units, null, crsType, scaleM * 1e6, roundNb, maxCoordinatesNumber, objMaxCoordinateNumber);
+
+				String outFile = "target/testout/tessgene/"+cntr+"_"+scaleM+".gpkg";
+				GeoData.save(units, outFile, GeoData.getCRS(inFile));
+			}
+	}
+
+}
