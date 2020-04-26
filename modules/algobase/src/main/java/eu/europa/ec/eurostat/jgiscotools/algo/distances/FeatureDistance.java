abe1870092ce8283519e5a251ef64534be2d8ee5
jgaffuri <jgaf@protonmail.com>
2020-04-26 11:24:24 +0200

    new module algobase

--- modules/algobase/src/main/java/eu/europa/ec/eurostat/jgiscotools/algo/distances/FeatureDistance.java
+++ modules/algobase/src/main/java/eu/europa/ec/eurostat/jgiscotools/algo/distances/FeatureDistance.java
@@ -3,7 +3,6 @@ package eu.europa.ec.eurostat.jgiscotools.algo.distances;
 import org.locationtech.jts.geom.Geometry;
 
 import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
-import eu.europa.ec.eurostat.jgiscotools.util.ProjectionUtil;
 
 /**
  * Distance for simple features.
@@ -12,19 +11,12 @@ import eu.europa.ec.eurostat.jgiscotools.util.ProjectionUtil;
  *
  */
 public class FeatureDistance implements Distance<Feature> {
-	private boolean projected = true;
 
-	public FeatureDistance() {}
-	public FeatureDistance(boolean projected){ this.projected = projected; }
+	public FeatureDistance(){}
 
 	public double get(Feature f1, Feature f2) {
 		Geometry g1 = f1.getGeometry();
 		Geometry g2 = f2.getGeometry();
-		if(!projected){
-			g1 = ProjectionUtil.toWebMercator(g1, ProjectionUtil.getWGS_84_CRS());
-			g2 = ProjectionUtil.toWebMercator(g2, ProjectionUtil.getWGS_84_CRS());
-		}
-		//System.out.println(g1.distance(g2));
 		return g1.distance(g2);
 	}
 
