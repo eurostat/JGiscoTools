c8bb09e815298b116562e84d72bb535def20672a
jgaffuri <jgaf@protonmail.com>
2020-12-11 15:40:37 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/routing/AccessibilityRoutingPaths.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/routing/AccessibilityRoutingPaths.java
@@ -32,7 +32,7 @@ import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
 import eu.europa.ec.eurostat.jgiscotools.feature.SimpleFeatureUtil;
 
 /**
- * Class to compute an accessibility grid to a set of points of interest, using a transport network.
+ * Class to compute accessiblity paths from grid cells to points of interest, using a transport network.
  * 
  * @author julien Gaffuri
  *
@@ -42,19 +42,16 @@ public class AccessibilityRoutingPaths {
 
 	//the grid cells
 	private Collection<Feature> cells = null;
-	//the grid cells
+	//the grid cells id
 	private String cellIdAtt = "GRD_ID";
 	//the grid resolution in m
 	private double resM = -1;
 	//the points of interest to measure the accessibility of
 	private Collection<Feature> pois = null;
+	//the number of POIs to consider
+	private int nbNearestPOIs = 4;
 	//the linear features composing the network
 	private Collection<Feature> networkSections = null;
-	//population attribute
-	private String populationAtt = null;
-
-	//threshold values to compute accessibility indicators
-	public double minDurAccMinT = 15;
 
 	//the weighter used to estimate the cost of each network section when computing shortest paths
 	private EdgeWeighter edgeWeighter = null;
@@ -85,12 +82,6 @@ public class AccessibilityRoutingPaths {
 	}
 
 
-
-
-	//the transport duration by grid cell
-	private Collection<Map<String, String>> cellData = null;
-	public Collection<Map<String, String>> getCellData() { return cellData; }
-
 	//the fastest route to one of the POIs for each grid cell
 	private Collection<Feature> routes = null;
 	public Collection<Feature> getRoutes() { return routes; }
@@ -98,14 +89,14 @@ public class AccessibilityRoutingPaths {
 
 
 
-	public AccessibilityRoutingPaths(Collection<Feature> cells, String cellIdAtt, double resM, Collection<Feature> pois, Collection<Feature> networkSections, String populationAtt, double minDurAccMinT) {
+	public AccessibilityRoutingPaths(Collection<Feature> cells, String cellIdAtt, double resM, Collection<Feature> pois, Collection<Feature> networkSections,int nbNearestPOIs) {
 		this.cells = cells;
 		this.cellIdAtt = cellIdAtt;
 		this.resM = resM;
 		this.pois = pois;
 		this.networkSections = networkSections;
-		this.populationAtt = populationAtt;
-		this.minDurAccMinT = minDurAccMinT;
+		this.nbNearestPOIs = nbNearestPOIs;
+
 	}
 
 
@@ -135,7 +126,6 @@ public class AccessibilityRoutingPaths {
 	}
 
 	//used to get nearest POIs from a location
-	public static int nbNearestPOIs = 4;
 	private static ItemDistance itemDist = new ItemDistance() {
 		@Override
 		public double distance(ItemBoundable item1, ItemBoundable item2) {
@@ -155,34 +145,21 @@ public class AccessibilityRoutingPaths {
 	//-30: no shortest path found to an hospital
 	public void compute() throws Exception {
 		//create output data structures
-		cellData = new ArrayList<>();
 		routes = new ArrayList<>();
 
 		//compute spatial indexes
 		getPoisInd();
 		getNetworkSectionsInd();
 
-		//make network sections feat
+		//get network sections feature type
 		SimpleFeatureType ft = SimpleFeatureUtil.getFeatureType(networkSections, "the_geom", null);
 
-		logger.info("Compute cell data...");
+		logger.info("Compute accessibility routing paths...");
 		for(Feature cell : cells) {
 
 			//get cell id
 			String cellId = cell.getAttribute(cellIdAtt).toString();
 			if(logger.isDebugEnabled()) logger.debug(cellId);
-
-			//build structure for cell data
-			HashMap<String, String> d = new HashMap<String, String>();
-			d.put(cellIdAtt, cellId);
-
-			/*/when cell contains at least one POI, set the duration to 0
-			if(getPoisInd().query(cell.getDefaultGeometry().getEnvelopeInternal()).size()>0) {
-				if(logger.isDebugEnabled()) logger.debug("POI in cell " + cellId);
-				d.put("dur_min", "0");
-				cellData.add(d);
-				continue;
-			}*/
 
 			if(logger.isDebugEnabled()) logger.debug("Get " + nbNearestPOIs + " nearest POIs");
 			Envelope netEnv = cell.getGeometry().getEnvelopeInternal(); netEnv.expandBy(1000);
@@ -203,36 +180,16 @@ public class AccessibilityRoutingPaths {
 			Routing rt = new Routing(net__, ft);
 			rt.setEdgeWeighter(getEdgeWeighter());
 
-			//get cell population, if provided
-			int population = 0;
-			if(populationAtt != null) {
-				Object pop = cell.getAttribute(populationAtt);
-				if(pop != null) {
-					population = (int)Double.parseDouble(pop.toString());
-					//d.put("pop_ind", "" + Util.round(getPopulationIndicator(population, popAccMinT), 3));
-				} else
-					;//d.put("pop_ind", "" + "-999");
-			}
-
-
 			//get cell centroid as origin point
 			//possible improvement: take another position depending on the network state inside the cell? Cell is supposed to be small enough?
 			Coordinate oC = cell.getGeometry().getCentroid().getCoordinate();
 			Node oN = rt.getNode(oC);
 			if(oN == null) {
 				logger.error("Could not find graph node around cell center: " + oC);
-				d.put("dur_min", "-10");
-				//d.put("dur_ind", "-10");
-				if(populationAtt != null) { /*d.put("acc_ind", "-10");*/ d.put("dmp_ind", "-10"); }
-				cellData.add(d);
 				continue;
 			}
 			if( ( (Point)oN.getObject() ).getCoordinate().distance(oC) > 1.3 * resM ) {
 				logger.trace("Cell center "+oC+" too far from clodest network node: " + oN.getObject());
-				d.put("dur_min", "-20");
-				//d.put("dur_ind", "-20");
-				if(populationAtt != null) { /*d.put("acc_ind", "-20");*/ d.put("dmp_ind", "-20"); }
-				cellData.add(d);
 				continue;
 			}
 
@@ -241,69 +198,39 @@ public class AccessibilityRoutingPaths {
 
 			//compute the routes to all POIs to get the best
 			if(logger.isDebugEnabled()) logger.debug("Compute routes to POIs. Nb=" + pois_.length);
-			Path pMin = null; double durMin = Double.MAX_VALUE;
 			for(Object poi_ : pois_) {
 				Feature poi = (Feature) poi_;
 				Coordinate dC = poi.getGeometry().getCentroid().getCoordinate();
 				//AStarShortestPathFinder pf = rt.getAStarShortestPathFinder(oC, dC);
 				//pf.calculate();
-				Path p = null; double cost;
+				Path p = null; double duration;
 				//include POI in path? Cell is supposed to be small enough?
 				try {
 					//p = pf.getPath();
 					Node dN = rt.getNode(dC);
 
 					if(dN == oN) {
-						durMin = 0;
-						pMin = null;
 						break;
 					}
 
 					p = pf.getPath(dN);
-					cost = pf.getCost(dN);
+					duration = pf.getCost(dN);
 					//For A*: see https://gis.stackexchange.com/questions/337968/how-to-get-path-cost-in/337972#337972
 				} catch (Exception e) {
 					//logger.warn("Could not compute path for cell " + cellId + ": " + e.getMessage());
 					continue;
 				}
-				if(p==null) continue;
-				//get best path
-				if(pMin==null || cost<durMin) {
-					pMin=p; durMin=cost;
-					if(durMin == 0) break;
-				}
-			}
 
-
-			if(durMin > 0 && pMin == null) {
-				if(logger.isDebugEnabled()) logger.debug("Could not find path to POI for cell " + cellId + " around " + oC);
-				d.put("dur_min", "-30");
-				//d.put("dur_ind", "-30");
-				if(populationAtt != null) { /*d.put("acc_ind", "-30");*/ d.put("dmp_ind", "-30"); }
-				cellData.add(d);
-				continue;
-			}
-
-
-			//store data at grid cell level
-			d.put("dur_min", "" + durMin);
-			//d.put("dur_ind", "" + Util.round(getAccessibilityIndicator(durMin, minDurAccMinT, maxDurAccMinT), 4));
-			//d.put("acc_ind", "" + Util.round(getPopulationAccessibilityIndicator(durMin, minDurAccMinT, maxDurAccMinT, population, popAccMinT), 4));
-			d.put("dmp_ind", "" + Util.round(getDelayMinPerson(durMin, minDurAccMinT, population), 4));
-
-			cellData.add(d);
-
-
-			if(pMin != null) {
 				//store route
-				Feature f = Routing.toFeature(pMin);
+				Feature f = Routing.toFeature(p);
 				f.setID(cellId);
 				f.setAttribute(cellIdAtt, cellId);
-				f.setAttribute("dur_min", durMin);
-				f.setAttribute("avSpeedKMPerH", Util.round(0.06 * f.getGeometry().getLength()/durMin, 2));
+				//TODO save also POI id
+				f.setAttribute("duration", duration);
+				f.setAttribute("avSpeedKMPerH", Util.round(0.06 * f.getGeometry().getLength()/duration, 2));
 				routes.add(f);
-			}
 
+			}
 		}
 	}
 
