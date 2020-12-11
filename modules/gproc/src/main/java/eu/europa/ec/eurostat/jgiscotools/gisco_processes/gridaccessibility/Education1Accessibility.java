4665bf1ebfcfc215fa14358221e477e9ed475c94
jgaffuri <jgaf@protonmail.com>
2020-12-11 15:10:32 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaccessibility/Education1Accessibility.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaccessibility/Education1Accessibility.java
@@ -27,69 +27,56 @@ import eu.europa.ec.eurostat.jgiscotools.routing.AccessibilityGrid.SpeedCalculat
 public class Education1Accessibility {
 	private static Logger logger = LogManager.getLogger(Education1Accessibility.class.getName());
 
+	//show where cross-border cooperation can improve accessibility
+
 	//use: -Xms2G -Xmx12G
 	public static void main(String[] args) throws Exception {
 		logger.info("Start");
 
 		//logger.setLevel(Level.ALL);
 
-		
 		String basePath = "E:/workspace/basic_services_accessibility/";
-		String outPath = basePath + "routing_paths/";
-		//String egPath = "E:/dissemination/shared-data/";
+		String outPath = basePath + "accessibility_output/";
 		CoordinateReferenceSystem crs = CRS.decode("EPSG:3035");
+		int resKM = 10;
 
 		//set the country id (set to null for all countries)
-		String cnt = null;
+		String cnt = "FR";
 
-		int resKM = 10;
 		logger.info("Load grid cells " + resKM + "km ...");
 		String cellIdAtt = "GRD_ID";
-		ArrayList<Feature> cells = GeoData.getFeatures(basePath + "grid/grid_"+resKM+"km.gpkg",null, cnt==null?null:CQL.toFilter("CNTR_ID = '"+cnt+"'"));
+		ArrayList<Feature> cells = GeoData.getFeatures(basePath + "input_data/grid_"+resKM+"km_surf.gpkg",null, cnt==null?null:CQL.toFilter("CNTR_ID = '"+cnt+"'"));
 		logger.info(cells.size() + " cells");
 
 
 		logger.info("Load network sections...");
-		Collection<Feature> networkSections = GeoData.getFeatures(egPath+ "ERM/gpkg/ERM_2019.1_LAEA/RoadL.gpkg", null, fil);
-
-		//ERM
-		//TODO add other transport networks (ferry, etc?)
-		//EXS Existence Category - RST Road Surface Type
-		Filter fil = CQL.toFilter("((EXS=28 OR EXS=0) AND (RST=1 OR RST=0))" + (cnt==null?"":" AND (ICC = '"+cnt+"')") );
-		Collection<Feature> networkSections = GeoData.getFeatures(egPath+ "ERM/gpkg/ERM_2019.1_LAEA/RoadL.gpkg", null, fil);
-		//Collection<Feature> networkSections = SHPUtil.getFeatures(egpath+"ERM/shp-gdb/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_14_15_16.shp", fil);
-		//networkSections.addAll( SHPUtil.getFeatures(egpath+"ERM/shp-gdb/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_984.shp", fil) );
-		//networkSections.addAll( SHPUtil.getFeatures(egpath+"ERM/shp-gdb/ERM_2019.1_shp_LAEA/Data/RoadL_RTT_0.shp", fil) );
-
+		//BD TOPO
+		Filter fil = CQL.toFilter("(NATURE is 'Sentier' AND NATURE is 'Chemin' AND NATURE is 'Piste Cyclable' AND NATURE is 'Escalier')");
+		Collection<Feature> networkSections = GeoData.getFeatures(basePath + "input_data/test_NMCA_FR_road_tn/roads.gpkg", null, fil);
 		SpeedCalculator sc = new SpeedCalculator() {
 			@Override
 			public double getSpeedKMPerHour(SimpleFeature sf) {
 				//estimate speed of a transport section of ERM/EGM based on attributes
 				//COR - Category of Road - 0 Unknown - 1 Motorway - 2 Road inside built-up area - 999 Other road (outside built-up area)
 				//RTT - Route Intended Use - 0 Unknown - 16 National motorway - 14 Primary route - 15 Secondary route - 984 Local route
-				String cor = sf.getAttribute("COR").toString();
-				if(cor==null) { logger.warn("No COR attribute for feature "+sf.getID()); return 0; };
-				String rtt = sf.getAttribute("RTT").toString();
-				if(rtt==null) { logger.warn("No RTT attribute for feature "+sf.getID()); return 0; };
+				//String cor = sf.getAttribute("COR").toString();
+				//if(cor==null) { logger.warn("No COR attribute for feature "+sf.getID()); return 0; };
+				//String rtt = sf.getAttribute("RTT").toString();
+				//if(rtt==null) { logger.warn("No RTT attribute for feature "+sf.getID()); return 0; };
 
 				//motorways
-				if("1".equals(cor) || "16".equals(rtt)) return 110.0;
+				//if("1".equals(cor) || "16".equals(rtt)) return 110.0;
 				//city roads
-				if("2".equals(cor)) return 50.0;
+				//if("2".equals(cor)) return 50.0;
 				//fast roads
-				if("14".equals(rtt) || "15".equals(rtt)) return 80.0;
+				//if("14".equals(rtt) || "15".equals(rtt)) return 80.0;
 				//local road
-				if("984".equals(rtt)) return 80.0;
+				//if("984".equals(rtt)) return 80.0;
 				return 50.0;
 			}
 		};
 		logger.info(networkSections.size() + " sections loaded.");
 
-
-
-
-
-		
 		final class Case {
 			String label, filter;
 			double minDurAccMinT;
@@ -108,7 +95,7 @@ public class Education1Accessibility {
 		}) {
 
 			logger.info("Load POIs " + c.label + "...");
-			ArrayList<Feature> pois = GeoData.getFeatures(egPath+"ERM/gpkg/ERM_2019.1_LAEA/GovservP.gpkg", null, CQL.toFilter("("+c.filter +")"+ (cnt==null?"":" AND (ICC = '"+cnt+"')") ));
+			ArrayList<Feature> pois = GeoData.getFeatures("ERM/gpkg/ERM_2019.1_LAEA/GovservP.gpkg", null, CQL.toFilter("("+c.filter +")"+ (cnt==null?"":" AND (ICC = '"+cnt+"')") ));
 			logger.info(pois.size() + " POIs");
 
 
