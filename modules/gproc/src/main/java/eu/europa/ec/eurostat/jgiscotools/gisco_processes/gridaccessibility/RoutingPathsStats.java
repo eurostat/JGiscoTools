4fde3c1e50e8e08070737f2e10a95795cb6efd8f
jgaffuri <jgaf@protonmail.com>
2020-12-13 15:38:50 +0100

    a

---
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaccessibility/RoutingPathsStats.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package eu.europa.ec.eurostat.jgiscotools.gisco_processes.gridaccessibility;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * @author clemoki
+ *
+ */
+public class RoutingPathsStats {
+	private static Logger logger = LogManager.getLogger(RoutingPathsStats.class.getName());
+
+	/** @param args **/
+	public static void main(String[] args) {
+		logger.info("Start");
+
+		//load paths
+		//index paths by grid cell
+		//
+
+		//for each grid cell
+		//get cell id
+		//get all paths from the grid cell
+		//get
+		
+		//Compute indicator 1 - Shortest transport time to the nearest service
+		//Compute indicator 2- Average transport time to the X nearest services
+		//Compute indicator 3 - Service capacity within X, Y, Z minutes
+
+
+		logger.info("End");
+	}
+
+}
