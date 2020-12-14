5ff8d3974364231869192e0ad84154479fd6fd6c
jgaffuri <jgaf@protonmail.com>
2020-12-14 16:05:05 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaccessibility/BasicServicesRoutingPaths.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaccessibility/BasicServicesRoutingPaths.java
@@ -46,10 +46,10 @@ public class BasicServicesRoutingPaths {
 		//TODO decompose by education type
 		//TODO make health as well
 		//TODO use more generalised road TN for healthcare
-		//String serviceType = "healthcare";
-		//ArrayList<Feature> pois = GeoData.getFeatures(basePath + "input_data/"+serviceType+"_services_LAEA.gpkg",null, cnt==null?null:CQL.toFilter("cc = '"+cnt+"'"));
-		String serviceType = "education_1"; int level = 1;
-		ArrayList<Feature> pois = GeoData.getFeatures(basePath + "input_data/"+serviceType+"_services_LAEA.gpkg",null, CQL.toFilter("levels LIKE '%"+level+"%'" + cnt==null?"":"AND cc = '"+cnt+"'") );
+		String serviceType = "healthcare";
+		ArrayList<Feature> pois = GeoData.getFeatures(basePath + "input_data/"+serviceType+"_services_LAEA.gpkg",null, cnt==null?null:CQL.toFilter("cc = '"+cnt+"'"));
+		//String serviceType = "education_1"; int level = 1;
+		//ArrayList<Feature> pois = GeoData.getFeatures(basePath + "input_data/"+serviceType+"_services_LAEA.gpkg",null, CQL.toFilter("levels LIKE '%"+level+"%'" + cnt==null?"":"AND cc = '"+cnt+"'") );
 		logger.info(pois.size() + " POIs");
 
 		logger.info("Load network sections...");
