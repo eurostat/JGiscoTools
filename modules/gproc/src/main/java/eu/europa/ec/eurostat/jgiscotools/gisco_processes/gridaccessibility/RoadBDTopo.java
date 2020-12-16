67a99590b934675f183b53e2c62469add9500970
jgaffuri <jgaf@protonmail.com>
2020-12-16 09:35:24 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaccessibility/RoadBDTopo.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaccessibility/RoadBDTopo.java
@@ -15,7 +15,7 @@ import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
 import eu.europa.ec.eurostat.jgiscotools.routing.SpeedCalculator;
 
 /**
- * @author clemoki
+ * @author julien Gaffuri
  *
  */
 public class RoadBDTopo {
@@ -31,10 +31,12 @@ public class RoadBDTopo {
 		} catch (CQLException e) { e.printStackTrace(); }
 		Collection<Feature> fs = GeoData.getFeatures(basePath + "input_data/test_NMCA_FR_SE_road_tn/roads.gpkg", null, fil);
 
-		if(computeCost) {
-			//TODO
-		}
-		
+		if(computeCost)
+			for(Feature f : fs) {
+				double speed = getSpeed(f.getAttribute("NATURE").toString(), f.getAttribute("IMPORTANCE").toString());
+				double duration = f.getGeometry().getLength() * 1000.0/60.0 * speed;
+				f.setAttribute("cost", duration);
+			}
 		return fs;
 	}
 
@@ -49,26 +51,29 @@ public class RoadBDTopo {
 				String nat = nat_==null?"":nat_.toString();
 				Object imp_ = sf.getAttribute("IMPORTANCE");
 				String imp = imp_==null?"":imp_.toString();
-
-				if("Autoroute".equals(nat)) return 110.0;
-				if("Quasi-autoroute".equals(nat)) return 100.0;
-				if("1".equals(imp)) return 90.0;
-				if("2".equals(imp)) return 80.0;
-				if("3".equals(imp)) return 70.0;
-				if("Bretelle".equals(nat)) return 70.0;
-				if("4".equals(imp)) return 60.0;
-				if("Route � 2 chauss�es".equals(nat)) return 60.0;
-				if("Route empierr�e".equals(nat)) return 35.0;
-				if("5".equals(imp)) return 50.0;
-				if("6".equals(imp)) return 5.0;
-
-				if("Route � 1 chauss�es".equals(nat)) return 60.0;
-				if("Piste cyclable".equals(nat)) return 5.0;
-
-				//System.err.println("Could not find speed for BD TOPO road section " + nat + " " + imp);
-				return 60.0;
+				return getSpeed(nat, imp);
 			}
 		};
+	}
+	
+	private static double getSpeed(String nat, String imp) {
+		if("Autoroute".equals(nat)) return 110.0;
+		if("Quasi-autoroute".equals(nat)) return 100.0;
+		if("1".equals(imp)) return 90.0;
+		if("2".equals(imp)) return 80.0;
+		if("3".equals(imp)) return 70.0;
+		if("Bretelle".equals(nat)) return 70.0;
+		if("4".equals(imp)) return 60.0;
+		if("Route � 2 chauss�es".equals(nat)) return 60.0;
+		if("Route empierr�e".equals(nat)) return 35.0;
+		if("5".equals(imp)) return 50.0;
+		if("6".equals(imp)) return 5.0;
+
+		if("Route � 1 chauss�es".equals(nat)) return 60.0;
+		if("Piste cyclable".equals(nat)) return 5.0;
+
+		//System.err.println("Could not find speed for BD TOPO road section " + nat + " " + imp);
+		return 60.0;
 	}
 
 }
