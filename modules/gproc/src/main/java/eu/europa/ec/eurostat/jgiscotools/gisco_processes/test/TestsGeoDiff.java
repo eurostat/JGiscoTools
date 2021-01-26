0279952b2d041770fc59a0318fa53fcbb5a85527
jgaffuri <jgaf@protonmail.com>
2021-01-26 09:49:49 +0100

    a

---
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/test/TestsGeoDiff.java
@@ -0,0 +1,38 @@
+package eu.europa.ec.eurostat.jgiscotools.gisco_processes.test;
+
+import java.util.Collection;
+
+import org.geotools.referencing.CRS;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
+import eu.europa.ec.eurostat.jgiscotools.geodiff.GeoDiff;
+import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
+
+public class TestsGeoDiff {
+
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Start");
+
+		System.out.println("load");
+		Collection<Feature> fs1 = GeoData.getFeatures("E:\\dissemination\\shared-data\\EBM\\gpkg\\EBM_2020_LAEA\\LAU.gpkg");
+		Collection<Feature> fs2 = GeoData.getFeatures("E:\\dissemination\\shared-data\\EBM\\gpkg\\EBM_2021_LAEA\\LAU.gpkg");
+
+		GeoDiff gd = new GeoDiff(fs1, fs2, 0.1);
+
+		System.out.println("compute diff");
+		Collection<Feature> diff = gd.getDifferences();
+		System.out.println(diff.size());
+
+		for(Feature f : diff) {
+			System.out.println(f.getGeometry().getGeometryType());
+		}
+
+		GeoData.save(diff, "C:\\Users\\gaffuju\\Desktop\\LAU", CRS.decode("EPSG:3035"));
+
+		System.out.println("End");
+	}
+
+}
