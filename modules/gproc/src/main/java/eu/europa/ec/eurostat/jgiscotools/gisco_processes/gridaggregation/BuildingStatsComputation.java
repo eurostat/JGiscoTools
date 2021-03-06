47e80f0e1299ad8c145a3f2e76a3701df14d41a6
jgaffuri <jgaf@protonmail.com>
2021-03-06 15:27:02 +0100

    a

---
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
@@ -0,0 +1,13 @@
+package eu.europa.ec.eurostat.jgiscotools.gisco_processes.gridaggregation;
+
+public class BuildingStatsComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+
+		//TODO use GridAggregator
+
+		System.out.println("End");
+	}
+
+}
