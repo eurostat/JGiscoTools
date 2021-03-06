12fa2346c6d723010924f7bd0e9fe433b145e77c
jgaffuri <jgaf@protonmail.com>
2021-03-06 15:39:37 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.locationtech.jts.geom.Geometry;
 
+import eu.europa.ec.eurostat.java4eurostat.io.CSV;
 import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
 import eu.europa.ec.eurostat.jgiscotools.geostat.GridAggregator;
 import eu.europa.ec.eurostat.jgiscotools.geostat.GridAggregator.FeatureContributionCalculator;
@@ -13,11 +14,15 @@ public class BuildingStatsComputation {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		//TODO load input data
-		Collection<Feature> cells = null;
-		Collection<Feature> fs = null;
+		String outPutFolder = "";  //TODO
 
-		//define feature contribution
+		System.out.println("Load cells...");
+		Collection<Feature> cells = null; //TODO
+
+		System.out.println("Load buildings...");
+		Collection<Feature> fs = null; //TODO
+
+		System.out.println("Define feature contribution calculator");
 		FeatureContributionCalculator fcc = new FeatureContributionCalculator() {
 			@Override
 			public double getContribution(Feature f, Geometry inter) {
@@ -32,8 +37,10 @@ public class BuildingStatsComputation {
 			GridAggregator ga = new GridAggregator(cells, "GRD_ID", fs, fcc);
 			ga.compute(true);
 
-			//save
-			//StatsHypercube sh = ga.getStats();
+			//TODO round stat values?
+
+			System.out.println("Save...");
+			CSV.save(ga.getStats(), "bu_res_area", outPutFolder + "/building_residential_area.csv");
 
 			System.out.println("End");
 	}
