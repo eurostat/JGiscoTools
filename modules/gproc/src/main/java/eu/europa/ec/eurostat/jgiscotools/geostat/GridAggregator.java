950cc46ecc82d881b72242d10d0f8713fe1abe1c
jgaffuri <jgaf@protonmail.com>
2021-03-06 15:28:49 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/geostat/GridAggregator.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/geostat/GridAggregator.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package eu.europa.ec.eurostat.jgiscotools.gisco_processes.gridaggregation;
+package eu.europa.ec.eurostat.jgiscotools.geostat;
 
 import java.util.Collection;
 import java.util.List;
--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
@@ -1,11 +1,14 @@
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes.gridaggregation;
 
+import eu.europa.ec.eurostat.jgiscotools.geostat.GridAggregator;
+
 public class BuildingStatsComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		//TODO use GridAggregator
+		//TODO
+		GridAggregator ga = new GridAggregator(null, null, null, null);
 
 		System.out.println("End");
 	}
