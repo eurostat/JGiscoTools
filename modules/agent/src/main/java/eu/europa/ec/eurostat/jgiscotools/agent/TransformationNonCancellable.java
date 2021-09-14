d255964d06d5bbe367972921de2f5858497cd66d
jgaffuri <jgaf@protonmail.com>
2021-09-14 13:22:02 +0200

    a

--- modules/agent/src/main/java/eu/europa/ec/eurostat/jgiscotools/agent/TransformationNonCancellable.java
+++ modules/agent/src/main/java/eu/europa/ec/eurostat/jgiscotools/agent/TransformationNonCancellable.java
@@ -5,6 +5,7 @@ package eu.europa.ec.eurostat.jgiscotools.agent;
 
 /**
  * A transformation, which cannot be cancelled.
+ * In theory, all transformations could be cancellable, as soon as the initial state can be stored. In practice, it is not always easy and implemented.
  * 
  * @author julien Gaffuri
  *
