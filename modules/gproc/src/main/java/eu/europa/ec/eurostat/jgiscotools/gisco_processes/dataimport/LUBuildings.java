34007ef23e4543c1129970885382de77336349f8
jgaffuri <jgaf@protonmail.com>
2021-10-07 19:38:27 +0200

    a

---
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/dataimport/LUBuildings.java
@@ -0,0 +1,12 @@
+package eu.europa.ec.eurostat.jgiscotools.gisco_processes.dataimport;
+
+public class LUBuildings {
+
+	public static void main(String[] args) {
+		
+		//https://catalog.inspire.geoportail.lu/geonetwork/srv/eng/catalog.search#/metadata/f9427e19-6d5f-4e03-adf2-21e84eeac7fc
+		//https://download.data.public.lu/resources/inspire-annex-iii-theme-buildings-building/20190429-152257/bu.building.gml
+		
+	}
+	
+}
