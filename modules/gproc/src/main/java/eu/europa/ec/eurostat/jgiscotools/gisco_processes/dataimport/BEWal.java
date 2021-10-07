326571f69918485bc29a7f741df4b64af36d5d1d
jgaffuri <jgaf@protonmail.com>
2021-10-07 20:53:26 +0200

    a

---
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/dataimport/BEWal.java
@@ -0,0 +1,7 @@
+package eu.europa.ec.eurostat.jgiscotools.gisco_processes.dataimport;
+
+public class BEWal {
+
+	//https://geoportail.wallonie.be/catalogue/b795de68-726c-4bdf-a62a-a42686aa5b6f.html
+
+}
