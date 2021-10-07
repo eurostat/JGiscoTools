0a80433f7c0f552ec7ec662043ecb215747df55d
jgaffuri <jgaf@protonmail.com>
2021-10-07 16:08:06 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/dataimport/IGNFrance.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/dataimport/IGNFrance.java
@@ -12,6 +12,11 @@ import org.apache.commons.compress.archivers.sevenz.SevenZFile;
 
 public class IGNFrance {
 
+	//https://geoservices.ign.fr/services-web-experts-topographie
+	//https://geoservices.ign.fr/services-web-inspire
+
+	//https://geoservices.ign.fr/telechargement
+
 	public static void main(String[] args) throws Exception {
 		System.out.println("Start");
 
@@ -20,36 +25,46 @@ public class IGNFrance {
 		//get 7z files
 		List<String> files = Files.walk(Paths.get(path))
 				.filter(p -> !Files.isDirectory(p))
-				.map(p -> p.toString().toLowerCase())
+				.map(p -> p.toString())
 				.filter(f -> f.endsWith("7z"))
 				.collect(Collectors.toList())
 				;
 
+		System.out.println(files.size());
+
 		//decompress
 		for(String file : files) {
-			System.out.println("Decompress "+file);
-			
-			SevenZFile sevenZFile = new SevenZFile(new File("test-documents.7z"));
-		    SevenZArchiveEntry entry = sevenZFile.getNextEntry();
-		    while(entry!=null){
-		        System.out.println(entry.getName());
-		        FileOutputStream out = new FileOutputStream(entry.getName());
-		        byte[] content = new byte[(int) entry.getSize()];
-		        sevenZFile.read(content, 0, content.length);
-		        out.write(content);
-		        out.close();
-		        entry = sevenZFile.getNextEntry();
-		    }
-		    sevenZFile.close();
-			
+			System.out.println("Decompress " + file);
+
+			String outFolder = path + file.toString().split("LAMB93_D")[1].replace(".7z","").replace("-","_") + "/";
+			new File(outFolder).mkdirs();
+			//System.out.println(outFolder);
+
+			SevenZFile sevenZFile = new SevenZFile(new File(file));
+			SevenZArchiveEntry entry = sevenZFile.getNextEntry();
+			while(entry!=null){
+				String en = entry.getName();
+				String fileClass = "BATIMENT";
+				if(!en.contains(fileClass)) {
+					entry = sevenZFile.getNextEntry();
+					continue;
+				}
+				System.out.println(en);
+
+				String ext = en.substring(en.length()-3, en.length());
+				FileOutputStream out = new FileOutputStream(outFolder + fileClass + "." + ext);
+				byte[] content = new byte[(int) entry.getSize()];
+				sevenZFile.read(content, 0, content.length);
+				out.write(content);
+				out.close();
+
+				entry = sevenZFile.getNextEntry();
+			}
+			sevenZFile.close();
+
 		}
-		
 
-
-		//https://geoservices.ign.fr/services-web-experts-topographie
-		//https://geoservices.ign.fr/services-web-inspire
-
-		//https://geoservices.ign.fr/telechargement
+		//reproject
 
 		System.out.println("End");
 	}
