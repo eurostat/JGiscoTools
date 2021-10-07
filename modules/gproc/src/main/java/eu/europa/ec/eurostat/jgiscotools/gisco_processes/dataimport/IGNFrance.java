725892ced19495c869f7b984ed002cfa15e6f233
jgaffuri <jgaf@protonmail.com>
2021-10-07 14:49:10 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/dataimport/IGNFrance.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/dataimport/IGNFrance.java
@@ -1,9 +1,14 @@
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes.dataimport;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
+import org.apache.commons.compress.archivers.sevenz.SevenZFile;
 
 public class IGNFrance {
 
@@ -11,13 +16,34 @@ public class IGNFrance {
 		System.out.println("Start");
 
 		String path = "/home/juju/Bureau/gisco/fr/bdtopo/";
+
+		//get 7z files
+		List<String> files = Files.walk(Paths.get(path))
+				.filter(p -> !Files.isDirectory(p))
+				.map(p -> p.toString().toLowerCase())
+				.filter(f -> f.endsWith("7z"))
+				.collect(Collectors.toList())
+				;
+
+		//decompress
+		for(String file : files) {
+			System.out.println("Decompress "+file);
+			
+			SevenZFile sevenZFile = new SevenZFile(new File("test-documents.7z"));
+		    SevenZArchiveEntry entry = sevenZFile.getNextEntry();
+		    while(entry!=null){
+		        System.out.println(entry.getName());
+		        FileOutputStream out = new FileOutputStream(entry.getName());
+		        byte[] content = new byte[(int) entry.getSize()];
+		        sevenZFile.read(content, 0, content.length);
+		        out.write(content);
+		        out.close();
+		        entry = sevenZFile.getNextEntry();
+		    }
+		    sevenZFile.close();
+			
+		}
 		
-		//get zip files
-		Stream<Path> zips = Files.find(Paths.get(path),
-		           Integer.MAX_VALUE,
-		           (path_, basicFileAttributes) -> path_.toFile().getName().matches("*.zip")
-		);
-		System.out.println(zips.count());
 
 
 		//https://geoservices.ign.fr/services-web-experts-topographie
