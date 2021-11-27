fa4ea6bfc01807257bf3ee0cdc5fd24a8f0f22a3
jgaffuri <jgaf@protonmail.com>
2021-11-27 10:41:46 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/INSEEGrid200m.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/INSEEGrid200m.java
@@ -18,30 +18,42 @@ public class INSEEGrid200m {
 	public static void main(String[] args) {
 		System.out.println("Start");
 		
+		String path = "/home/juju/Bureau/gisco/cnt/fr/fr_200m/";
+		
 		System.out.println("Format");
-		ArrayList<Map<String, String>> data = CSVUtil.load("/home/juju/Bureau/gisco/cnt/fr/fr_200m/Filosofi2015_carreaux_200m_metropole.csv");
+		ArrayList<Map<String, String>> data = CSVUtil.load(path + "Filosofi2015_carreaux_200m_metropole.csv");
 		System.out.println(data.size());
 		System.out.println(data.get(0).keySet());
 
+		System.out.println("Remove colums");
 		CSVUtil.removeColumn(data,
-				"IdINSPIRE",
 				"Id_carr1km",
 				"I_est_cr",
 				"Id_carr_n",
 				"Groupe",
 				"Depcom",
 				"I_pauv",
-				"Id_car2010");
+				"Id_car2010",
+				"I_est_1km");
 
-		//set x,y
+		System.out.println("Set x,y");
 		for(Map<String, String> c : data) {
-			String id = c.get("IdINSPIRE");
-			System.out.println(id);
+			String s = c.get("IdINSPIRE");
+			//CRS3035RES200mN2940600E3844600
+			s = s.split("mN")[1];
+			c.put("x", Integer.parseInt(s.split("E")[0])+"" );
+			c.put("x", Integer.parseInt(s.split("E")[1])+"" );
 		}
 
-		//
+		System.out.println("Remove colums");
 		CSVUtil.removeColumn(data, "IdINSPIRE");
 
+		System.out.println(data.size());
+		System.out.println(data.get(0).keySet());
+
+		System.out.println("save");
+		CSVUtil.save(data, path + "Filosofi2015_prepared.csv");
+		
 		System.out.println("End");
 	}
 
