5f7bd9c14794fad249ed87fb87baab85f560e231
jgaffuri <jgaf@protonmail.com>
2021-11-27 10:30:42 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/INSEEGrid200m.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/INSEEGrid200m.java
@@ -14,18 +14,32 @@ import eu.europa.ec.eurostat.jgiscotools.io.CSVUtil;
  */
 public class INSEEGrid200m {
 
+	//-Xms4g -Xmx16g
 	public static void main(String[] args) {
 		System.out.println("Start");
 		
 		System.out.println("Format");
-		ArrayList<Map<String, String>> data = CSVUtil.load("/home/juju/Bureau/gisco/cnt/fr/fr_200m/");
+		ArrayList<Map<String, String>> data = CSVUtil.load("/home/juju/Bureau/gisco/cnt/fr/fr_200m/Filosofi2015_carreaux_200m_metropole.csv");
 		System.out.println(data.size());
 		System.out.println(data.get(0).keySet());
-		System.out.println(data.get(0).keySet().size());
 
+		CSVUtil.removeColumn(data,
+				"IdINSPIRE",
+				"Id_carr1km",
+				"I_est_cr",
+				"Id_carr_n",
+				"Groupe",
+				"Depcom",
+				"I_pauv",
+				"Id_car2010");
+
+
+		//get geom
+		//"IdINSPIRE"
+
+		CSVUtil.removeColumn(data"IdINSPIRE")
 
 		System.out.println("End");
 	}
-	
-	
+
 }
