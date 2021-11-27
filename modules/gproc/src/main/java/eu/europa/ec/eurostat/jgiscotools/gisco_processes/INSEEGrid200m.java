846bb47e1c6b9e106e7197341d381f5c5f6e1dfe
jgaffuri <jgaf@protonmail.com>
2021-11-27 10:17:16 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/INSEEGrid200m.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/INSEEGrid200m.java
@@ -3,10 +3,29 @@
  */
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes;
 
+import java.util.ArrayList;
+import java.util.Map;
+
+import eu.europa.ec.eurostat.jgiscotools.io.CSVUtil;
+
 /**
- * @author juju
+ * @author julien Gaffuri
  *
  */
 public class INSEEGrid200m {
 
+	public static void main(String[] args) {
+		System.out.println("Start");
+		
+		System.out.println("Format");
+		ArrayList<Map<String, String>> data = CSVUtil.load("/home/juju/Bureau/gisco/cnt/fr/fr_200m/");
+		System.out.println(data.size());
+		System.out.println(data.get(0).keySet());
+		System.out.println(data.get(0).keySet().size());
+		
+		
+		System.out.println("End");
+	}
+	
+	
 }
