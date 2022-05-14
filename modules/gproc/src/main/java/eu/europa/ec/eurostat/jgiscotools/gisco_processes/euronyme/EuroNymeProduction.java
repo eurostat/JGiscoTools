a6978709148a4e1e2dff2594f2be66c73c968c3d
jgaffuri <jgaf@protonmail.com>
2022-05-14 20:19:25 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/euronyme/EuroNymeProduction.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/euronyme/EuroNymeProduction.java
@@ -3,7 +3,10 @@
  */
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes.euronyme;
 
+import java.util.ArrayList;
+
 import eu.europa.ec.eurostat.jgiscotools.deprecated.GeoUtils;
+import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
 import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
 
 /**
@@ -24,7 +27,7 @@ public class EuroNymeProduction {
 		
 		//load input data
 		String erm = "/home/juju/Bureau/gisco/geodata/euro-regional-map-gpkg/data/OpenEuroRegionalMap.gpkg";
-		GeoData.getFeatures(erm, "id");
+		ArrayList<Feature> fs = GeoData.getFeatures(erm, "id");
 		//BuiltupA BuiltupP EBM_NAM
 		
 		
