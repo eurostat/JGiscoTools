c976ff71399011fe830f516d3146d68df5d85917
jgaffuri <jgaf@protonmail.com>
2022-07-24 11:22:09 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/EurAccessibility.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/EurAccessibility.java
@@ -77,7 +77,7 @@ public class EurAccessibility {
 
 		for (int res : resolutions) {
 			logger.info("Aggregate " + res + "m");
-			ArrayList<Map<String, String>> out = GridMultiResolutionProduction.gridAggregation(data, "GRD_ID", res, 10000);
+			ArrayList<Map<String, String>> out = GridMultiResolutionProduction.gridAggregation(data, "GRD_ID", res, 10000, true);
 			logger.info(out.size());
 
 			logger.info("Save");
