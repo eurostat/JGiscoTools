c273815188b40f733ebb2989f189068aa9092a27
jgaffuri <jgaf@protonmail.com>
2022-09-30 10:18:54 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/RailAccessibility.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/RailAccessibility.java
@@ -2,19 +2,21 @@ package eu.europa.ec.eurostat.jgiscotools.gisco_processes.gridvizprep;
 
 import java.awt.image.RenderedImage;
 import java.io.File;
-import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.geotools.coverage.grid.GridCoordinates2D;
 import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverage.grid.GridEnvelope2D;
 import org.geotools.coverage.grid.io.AbstractGridFormat;
 import org.geotools.coverage.grid.io.GridCoverage2DReader;
 import org.geotools.coverage.grid.io.GridFormatFinder;
 import org.opengis.geometry.Envelope;
-import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 public class RailAccessibility {
 	static Logger logger = LogManager.getLogger(RailAccessibility.class.getName());
+
+	//https://docs.geotools.org/stable/userguide/library/coverage/geotiff.html
 
 	// the target resolutions
 	private static int[] resolutions = new int[] { 1000, 2000, 5000, 10000, 20000, 50000, 100000 };
@@ -38,17 +40,21 @@ public class RailAccessibility {
 
 		//GeoTiffReader reader = new GeoTiffReader(file, new Hints(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE));
 
-		
-		GridCoverage2D coverage = (GridCoverage2D) reader.read(null);
-		//CoordinateReferenceSystem crs = coverage.getCoordinateReferenceSystem2D();
-		//System.out.println(crs);
-		Envelope env = coverage.getEnvelope();
-		RenderedImage image = coverage.getRenderedImage();
 
-		System.out.println(image);
-		System.out.println(env);
-		System.out.println(image.getHeight());
-		System.out.println(image.getPropertyNames());
+		GridCoverage2D coverage = (GridCoverage2D) reader.read(null);
+		Envelope env = coverage.getEnvelope();
+		GridEnvelope2D bn = coverage.getGridGeometry().getGridRange2D();
+
+		double[] dest = new double[1];
+		for(int i=0; i<bn.width; i++)
+			for(int j=0; j<bn.height; j++){
+				coverage.evaluate(new GridCoordinates2D(i,j), dest);
+				double d = dest[0];
+				if(d==2.147483647E9) continue;
+				if(d==0.0) continue;
+				//System.out.println(i+" "+j+" --- "+d);
+			}		
+
 	}
 
 }
