08a5d09dc905e7568bf2dda75a7068bf81b32e80
jgaffuri <jgaf@protonmail.com>
2022-10-04 10:12:15 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/EurElevation.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/EurElevation.java
@@ -52,85 +52,6 @@ public class EurElevation {
 
 
 
-
-	//get coverage from tiff file
-	public static GridCoverage2D getGeoTIFFCoverage(String inTiff) {
-		/*
-		File file = new File(inTiff);
-		AbstractGridFormat format = GridFormatFinder.findFormat( file );
-		GridCoverage2DReader reader = format.getReader( file );
-		GridCoverage2D coverage = null;
-		try {
-			coverage = (GridCoverage2D) reader.read(null);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return coverage;
-		 */
-
-		try {
-			GeoTiffReader reader = new GeoTiffReader(new File(inTiff)/*, new Hints(Hints.FORCE_LONGITUDE_FIRST_AXIS_ORDER, Boolean.TRUE)*/);
-			return reader.read(null);
-		} catch (Exception e) { e.printStackTrace(); }
-		return null;
-	}
-
-	interface SkipFunction { boolean skip(int[] v); }
-
-	public static ArrayList<Map<String, String>> loadCells(GridCoverage2D coverage, String[] outProps, SkipFunction skip) {
-
-		//get envelopes
-		Envelope envG = coverage.getEnvelope();
-		GridEnvelope2D env = coverage.getGridGeometry().getGridRange2D();
-		//System.out.println(envG);
-		//System.out.println(env);
-
-		//compute and check resolution
-		double resX = (envG.getMaximum(0) - envG.getMinimum(0)) / env.getWidth();
-		double resY = (envG.getMaximum(1) - envG.getMinimum(1)) / env.getHeight();
-		if(resX != resY)
-			throw new Error("Different X/Y resolutions: "+resX + " and "+resY);
-		//System.out.println(resX);
-
-		//output
-		ArrayList<Map<String, String>> out = new ArrayList<>();
-
-		int nb = outProps.length;
-		int[] v = new int[nb];
-
-		for(int i=0; i<env.width; i++){
-		//IntStream.rangeClosed(0, env.width -1).parallel().forEach(i -> {
-			for(int j=0; j<env.height; j++){
-
-				//get cell values
-				coverage.evaluate(new GridCoordinates2D(i,j), v);
-
-				//check if to keep
-				if(skip.skip(v)) continue;
-
-				//prepare cell
-				Map<String, String> d = new HashMap<>();
-
-				//set cell code
-				int x = (int)(envG.getMinimum(0) + i*resX);
-				int y = (int)(envG.getMaximum(1) - (j+1)*resX);
-				d.put("GRD_ID", "CRS3035RES"+((int)resX)+"m"+"N"+y+"E"+x);
-				//d.put("x", x + "");
-				//d.put("y", y + "");
-
-				//set cell values
-				for(int p=0; p<nb; p++) {
-					d.put(outProps[p], v[p] + "");
-				}
-
-				out.add(d);
-			}
-		}//);
-		return out;
-	}
-
-
-
 	// tile all resolutions
 	private static void tiling() {
 
@@ -140,10 +61,10 @@ public class EurElevation {
 			String f = basePath + res+".tif";
 
 			logger.info("Load geoTiff");
-			GridCoverage2D coverage = getGeoTIFFCoverage(f);
+			GridCoverage2D coverage = GeoTiffUtil.getGeoTIFFCoverage(f);
 
 			logger.info("Load grid cells");
-			ArrayList<Map<String, String>> cells = loadCells(coverage, new String[] {"elevation"}, (v)->{ return v[0]==0 || Double.isNaN(v[0]); } );
+			ArrayList<Map<String, String>> cells = GeoTiffUtil.loadCells(coverage, new String[] {"elevation"}, (v)->{ return v[0]==0 || Double.isNaN(v[0]); } );
 			logger.info(cells.size());
 
 			//logger.info("Round");
