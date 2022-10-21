a9283de37833b2b25011b860af38544e789ee52c
jgaffuri <jgaf@protonmail.com>
2022-10-21 14:23:55 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/CLC2NUTSAggregation.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/CLC2NUTSAggregation.java
@@ -38,7 +38,9 @@ public class CLC2NUTSAggregation {
 		for(int nutsLevel=3; nutsLevel>=0; nutsLevel--) {
 
 			logger.info("Load NUTS level " + nutsLevel);
-			ArrayList<Feature> nuts = GeoData.getFeatures(nutsFile, "NUTS_ID", CQL.toFilter("STAT_LEVL_CODE='"+nutsLevel+"'")); // AND SHAPE_AREA<0.01
+			ArrayList<Feature> nuts = GeoData.getFeatures(nutsFile, "NUTS_ID", CQL.toFilter("STAT_LEVL_CODE='"+nutsLevel+"' AND NUTS_ID LIKE 'FR%'"));
+			// AND NUTS_ID LIKE 'FR%'
+			// AND SHAPE_AREA<0.01
 			//[OBJECTID, SHAPE_LEN, STAT_LEVL_CODE, id, NUTS_ID, SHAPE_AREA]
 			logger.info(nuts.size());
 
