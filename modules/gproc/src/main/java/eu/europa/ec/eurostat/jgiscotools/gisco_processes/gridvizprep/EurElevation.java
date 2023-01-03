6e0a75ef6cd7ff9cff785cef6af999843137ac0f
jgaffuri <jgaf@proton.me>
2023-01-03 22:00:16 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/EurElevation.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/EurElevation.java
@@ -24,7 +24,7 @@ public class EurElevation {
 	public static void main(String[] args) throws Throwable {
 		logger.info("Start");
 
-		resampling();
+		//resampling();
 
 		tiling(Format.PARQUET, CompressionCodecName.GZIP, 256);
 
@@ -49,7 +49,7 @@ public class EurElevation {
 			GridCoverage2D coverage = GeoTiffUtil.getGeoTIFFCoverage(f);
 
 			logger.info("Load grid cells");
-			ArrayList<Map<String, String>> cells = GeoTiffUtil.loadCells(coverage, new String[] {"elevation"}, (v)->{ return v[0]==0 || Double.isNaN(v[0]); }, true );
+			ArrayList<Map<String, String>> cells = GeoTiffUtil.loadCells(coverage, new String[] {"elevation"}, (v)->{ return v[0]==0 || Double.isNaN(v[0]); }, false );
 			logger.info(cells.size());
 
 			//logger.info("Round");
