34f79355940c4e6b41c39bf053d165f559fe0d76
jgaffuri <jgaf@proton.me>
2023-01-24 12:50:36 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/EBMvalidation/GeodiffTests.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/EBMvalidation/GeodiffTests.java
@@ -8,11 +8,17 @@ import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
 public class GeodiffTests {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 
-		
+
+		ArrayList<Feature> fs1 = GeoData.getFeatures("/home/juju/Bureau/gisco/geodata/EBM/2022/StatisticalUnits/NUTS_3.gpkg");
+		System.out.println(fs1.size());
+
 		ArrayList<Feature> fs2 = GeoData.getFeatures("/home/juju/Bureau/gisco/geodata/EBM/2023/StatisticalUnits/NUTS_3.gpkg");
 		System.out.println(fs2.size());
-		
+
+
+		System.out.println("End");
 	}
 
 }
