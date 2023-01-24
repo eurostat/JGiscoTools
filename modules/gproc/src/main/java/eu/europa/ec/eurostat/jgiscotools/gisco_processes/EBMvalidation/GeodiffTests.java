e2743862c8dccadac297ec2c5d4525bbce2151df
jgaffuri <jgaf@proton.me>
2023-01-24 15:23:00 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/EBMvalidation/GeodiffTests.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/EBMvalidation/GeodiffTests.java
@@ -9,18 +9,22 @@ public class GeodiffTests {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		SimpleFeatureType sc1 = GeoData.getSchema("/home/juju/Bureau/gisco/geodata/EBM/2022/StatisticalUnits/NUTS_3.gpkg");
+		String t = "EBM_A";
+
+		SimpleFeatureType sc1 = GeoData.getSchema("/home/juju/Bureau/gisco/geodata/EBM/2022/BasicGeometry/"+t+".gpkg");
 		System.out.println(sc1);
-		SimpleFeatureType sc2 = GeoData.getSchema("/home/juju/Bureau/gisco/geodata/EBM/2022/StatisticalUnits/NUTS_3.gpkg");
+		System.out.println( sc1.getGeometryDescriptor() );
+
+		SimpleFeatureType sc2 = GeoData.getSchema("/home/juju/Bureau/gisco/geodata/EBM/2023/BasicGeometry/"+t+".gpkg");
 		System.out.println(sc2);
+		System.out.println( sc2.getGeometryDescriptor() );
 
 
-		//ArrayList<Feature> fs1 = GeoData.getFeatures("/home/juju/Bureau/gisco/geodata/EBM/2022/StatisticalUnits/NUTS_3.gpkg");
+		//ArrayList<Feature> fs1 = GeoData.getFeatures("/home/juju/Bureau/gisco/geodata/EBM/2022/BasicGeometry/"+t+".gpkg");
 		//System.out.println(fs1.size());
 
-		//ArrayList<Feature> fs2 = GeoData.getFeatures("/home/juju/Bureau/gisco/geodata/EBM/2023/StatisticalUnits/NUTS_3.gpkg");
+		//ArrayList<Feature> fs2 = GeoData.getFeatures("/home/juju/Bureau/gisco/geodata/EBM/2023/BasicGeometry/"+t+".gpkg");
 		//System.out.println(fs2.size());
-
 
 		System.out.println("End");
 	}
