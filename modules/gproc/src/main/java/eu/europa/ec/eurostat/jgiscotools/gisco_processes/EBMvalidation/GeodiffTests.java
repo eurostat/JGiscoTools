a1dd501ceec6de7117fb1fe82501bd98ed5c624b
jgaffuri <jgaf@proton.me>
2023-01-25 09:30:24 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/EBMvalidation/GeodiffTests.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/EBMvalidation/GeodiffTests.java
@@ -13,47 +13,50 @@ public class GeodiffTests {
 
 	public static void main(String[] args) {
 		System.out.println("Start");
-		
+
 		String inFolder = "/home/juju/Bureau/gisco/geodata/EBM/";
 		String outFolder = "/home/juju/Bureau/gisco/EBM_validation/geodiff/";
 
-		String t = "EBM_P";
+		for(String t : new String[] { "NUTS_1", "NUTS_2", "NUTS_3", "LAU", "EBM_P", "EBM_A" }) {
+			System.out.println("GeoDiff of " + t);
 
-		SimpleFeatureType sc1 = GeoData.getSchema(inFolder + "2022_"+t+".gpkg");
-		//System.out.println(sc1);
-		//System.out.println( sc1.getGeometryDescriptor() );
+			SimpleFeatureType sc1 = GeoData.getSchema(inFolder + "2022_"+t+".gpkg");
+			//System.out.println(sc1);
+			//System.out.println( sc1.getGeometryDescriptor() );
 
-		SimpleFeatureType sc2 = GeoData.getSchema(inFolder + "2023_"+t+".gpkg");
-		//System.out.println(sc2);
-		//System.out.println( sc2.getGeometryDescriptor() );
+			SimpleFeatureType sc2 = GeoData.getSchema(inFolder + "2023_"+t+".gpkg");
+			//System.out.println(sc2);
+			//System.out.println( sc2.getGeometryDescriptor() );
 
-		ArrayList<Feature> fs1 = GeoData.getFeatures(inFolder + "2022_"+t+".gpkg", "inspireId");
-		System.out.println(fs1.size());
+			ArrayList<Feature> fs1 = GeoData.getFeatures(inFolder + "2022_"+t+".gpkg", "inspireId");
+			System.out.println(fs1.size());
 
-		ArrayList<Feature> fs2 = GeoData.getFeatures(inFolder + "2023_"+t+".gpkg", "inspireId");
-		System.out.println(fs2.size());
-
-
-		GeoDiff gd = new GeoDiff(fs1, fs2, 20);
-		//SimpleFeatureTypeImpl BasicGeometry/EBM_P identified extends Feature(geom:geom,OBJECTID:OBJECTID,inspireId:inspireId,beginLifespanVersion:beginLifespanVersion,ICC:ICC,SHN:SHN)
-		//SimpleFeatureTypeImpl BasicGeometry/EBM_P identified extends Feature(geom:geom,OBJECTID:OBJECTID,inspireId:inspireId,beginLifespanVersion:beginLifespanVersion,ICC:ICC,SHN:SHN)
-		gd.setAttributesToIgnore("OBJECTID");
+			ArrayList<Feature> fs2 = GeoData.getFeatures(inFolder + "2023_"+t+".gpkg", "inspireId");
+			System.out.println(fs2.size());
 
 
-		//
-		if(gd.getDifferences().size() > 0)
-			GeoData.save(gd.getDifferences(), outFolder+t+"/geodiff.gpkg", sc1.getCoordinateReferenceSystem());
-		if(gd.getHausdorffGeomDifferences().size() > 0)
-			GeoData.save(gd.getHausdorffGeomDifferences(), outFolder+t+"/hausdorf.gpkg", sc1.getCoordinateReferenceSystem());
-		if(gd.getIdentical().size() > 0)
-			GeoData.save(gd.getIdentical(), outFolder+t+"/identical.gpkg", sc1.getCoordinateReferenceSystem());
-		if(gd.getGeomDifferences().size() > 0)
-			GeoData.save(gd.getGeomDifferences(), outFolder+t+"/geomDifferences.gpkg", sc1.getCoordinateReferenceSystem());
+			GeoDiff gd = new GeoDiff(fs1, fs2, 20);
+			//SimpleFeatureTypeImpl BasicGeometry/EBM_P identified extends Feature(geom:geom,OBJECTID:OBJECTID,inspireId:inspireId,beginLifespanVersion:beginLifespanVersion,ICC:ICC,SHN:SHN)
+			//SimpleFeatureTypeImpl BasicGeometry/EBM_P identified extends Feature(geom:geom,OBJECTID:OBJECTID,inspireId:inspireId,beginLifespanVersion:beginLifespanVersion,ICC:ICC,SHN:SHN)
+			gd.setAttributesToIgnore("OBJECTID");
 
-		Collection<Feature> si = GeoDiff.findIdStabilityIssues(gd.getDifferences(), 20);
-		System.out.println("Stability issues: " + si.size());
-		if(si.size() > 0)
-			GeoData.save(si, outFolder+t+"/idStabIssues.gpkg", sc1.getCoordinateReferenceSystem());
+
+			//
+			if(gd.getDifferences().size() > 0)
+				GeoData.save(gd.getDifferences(), outFolder+t+"/geodiff.gpkg", sc1.getCoordinateReferenceSystem());
+			if(gd.getHausdorffGeomDifferences().size() > 0)
+				GeoData.save(gd.getHausdorffGeomDifferences(), outFolder+t+"/hausdorf.gpkg", sc1.getCoordinateReferenceSystem());
+			if(gd.getIdentical().size() > 0)
+				GeoData.save(gd.getIdentical(), outFolder+t+"/identical.gpkg", sc1.getCoordinateReferenceSystem());
+			if(gd.getGeomDifferences().size() > 0)
+				GeoData.save(gd.getGeomDifferences(), outFolder+t+"/geomDifferences.gpkg", sc1.getCoordinateReferenceSystem());
+
+			Collection<Feature> si = GeoDiff.findIdStabilityIssues(gd.getDifferences(), 20);
+			System.out.println("Stability issues: " + si.size());
+			if(si.size() > 0)
+				GeoData.save(si, outFolder+t+"/idStabIssues.gpkg", sc1.getCoordinateReferenceSystem());
+
+		}
 
 		System.out.println("End");
 	}
