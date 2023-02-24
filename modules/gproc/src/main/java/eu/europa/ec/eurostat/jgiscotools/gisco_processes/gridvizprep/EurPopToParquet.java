dcd0beab2499126f4d4e93040b9fcf88f42dcfa1
jgaffuri <jgaf@proton.me>
2023-02-24 23:50:03 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/EurPopToParquet.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/EurPopToParquet.java
@@ -3,6 +3,8 @@
  */
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes.gridvizprep;
 
+import java.io.File;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,14 +20,18 @@ public class EurPopToParquet {
 
 	//the target resolutions
 	private static int[] resolutions = new int[] { 100000, 50000, 20000, 10000, 5000, 2000, 1000 };
-	private static String path = "/home/juju/Bureau/gisco/grid_pop/";
+	private static String path = "/home/juju/Bureau/a/";
 
 	public static void main(String[] args) {
 		logger.info("Start");
 
 		for(int r : resolutions) {
 			logger.info("Resolution " + r + "m");		
-			ParquetUtil.convertCSVToParquet(path + "pop_"+r+"m.csv", path, "___pop_"+r+"m.parquet", "GZIP");
+
+			String f = path + "pop_"+r+"m.csv";
+			ParquetUtil.convertCSVToParquet(f, path, "population", "GZIP");
+
+			new File(path + "population.parquet").renameTo(new File(path + "pop_"+r+"m.parquet"));
 		}
 
 		logger.info("End");
