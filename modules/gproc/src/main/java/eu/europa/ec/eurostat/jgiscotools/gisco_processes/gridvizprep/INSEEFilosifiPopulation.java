37825ee423ca0a5a7bdc290870c4083e2b8bef1b
jgaffuri <jgaf@proton.me>
2023-03-19 22:11:54 +0100

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/INSEEFilosifiPopulation.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridvizprep/INSEEFilosifiPopulation.java
@@ -27,7 +27,8 @@ public class INSEEFilosifiPopulation {
 	public static void main(String[] args) {
 		logger.info("Start");
 
-		prepare();
+		//prepare2015();
+		prepare2017();
 
 		//aggregate();
 		//tiling();
@@ -35,7 +36,8 @@ public class INSEEFilosifiPopulation {
 		logger.info("End");
 	}
 
-	private static void prepare() {
+	
+	private static void prepare2015() {
 
 		// 2015
 		// IdINSPIRE
@@ -43,13 +45,14 @@ public class INSEEFilosifiPopulation {
 
 		logger.info("Load 2015");
 		ArrayList<Map<String, String>> data = CSVUtil.load(basePath + "2015/Filosofi2015_carreaux_200m_metropole.csv");
-		logger.info(data.size());
-		logger.info(data.get(0).keySet());
+		//logger.info(data.size());
+		//logger.info(data.get(0).keySet());
 
 		logger.info("Remove colums");
-		CSVUtil.removeColumn(data, "Id_carr1km", "Id_carr_n", "Groupe", "Depcom", "I_pauv", "Id_car2010", "I_est_1km");
+		CSVUtil.removeColumn(data, "Id_carr1km", "Id_carr_n", "Groupe", "Depcom", "I_pauv", "Id_car2010", "Men", "Men_pauv", "Men_1ind", "Men_5ind", "Men_prop", "Men_fmp", "Ind_snv", "Men_surf", "Men_coll", "Men_mais", "Log_av45", "Log_45_70", "Log_70_90", "Log_ap90", "Log_inc", "Log_soc", "Ind_0_3", "Ind_4_5", "Ind_6_10", "Ind_11_17", "Ind_18_24", "Ind_25_39", "Ind_40_54", "Ind_55_64", "Ind_65_79", "Ind_80p", "Ind_inc", "I_est_1km");
 
 		logger.info("Rename colums");
+		CSVUtil.renameColumn(data, "Ind", "Ind_2015");
 		CSVUtil.renameColumn(data, "IdINSPIRE", "GRD_ID");
 		CSVUtil.renameColumn(data, "I_est_cr", "imputed");
 
@@ -57,18 +60,20 @@ public class INSEEFilosifiPopulation {
 		logger.info(data.get(0).keySet());
 
 		//logger.info("save 2015");
-		//CSVUtil.save(data, basePath + "out/2015_prepared.csv");
+		CSVUtil.save(data, basePath + "out/2015_prepared.csv");
+	}
+	
 
+	private static void prepare2017() {
 
-/*
 		// 2017
 		// Idcar_200m
 		// Ind - Nombre d’individus
 
 		logger.info("Load 2017");
-		data = CSVUtil.load(basePath + "2017/Filosofi2017_carreaux_200m_met.csv");
-		//logger.info(data.size());
-		//logger.info(data.get(0).keySet());
+		ArrayList<Map<String, String>> data = CSVUtil.load(basePath + "2017/Filosofi2017_carreaux_200m_met.csv");
+		logger.info(data.size());
+		logger.info(data.get(0).keySet());
 
 		logger.info("Remove colums");
 		CSVUtil.removeColumn(data, "Idcar_1km", "Idcar_nat", "I_est_1km", "Groupe", "lcog_geo");
@@ -95,9 +100,9 @@ public class INSEEFilosifiPopulation {
 
 		logger.info(data.size());
 		logger.info(data.get(0).keySet());
-*/
+		 */
 
-//logger.info("save 2017");
+		//logger.info("save 2017");
 		//CSVUtil.save(data, basePath + "out/2017_prepared.csv");
 	}
 
@@ -149,7 +154,7 @@ public class INSEEFilosifiPopulation {
 			gst.saveTilingInfoJSON(outpath, GridTiler.Format.CSV, "Filosofi population resolution " + res + "m");
 
 		}
-		*/
+		 */
 	}
 
 }
