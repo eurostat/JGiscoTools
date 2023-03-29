fb9d8bdca5dcf3663f5a6db5ebb9884ffb5f78ce
jgaffuri <jgaf@proton.me>
2023-03-29 15:17:18 +0200

    a

---
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridproduction/ZZZJoin2021Population.java
@@ -0,0 +1,15 @@
+package eu.europa.ec.eurostat.jgiscotools.gisco_processes.gridproduction;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ZZZJoin2021Population {
+	static Logger logger = LogManager.getLogger(ZZZJoin2021Population.class.getName());
+
+	//use: -Xms2G -Xmx6G
+	public static void main(String[] args) throws Exception {
+		logger.info("Start");
+
+	}
+
+}
