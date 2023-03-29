4f81be5225beb66d7715debe034aa80959b79ac4
jgaffuri <jgaf@proton.me>
2023-03-29 15:41:57 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridproduction/ZZZJoin2021Population.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridproduction/ZZZJoin2021Population.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
 import eu.europa.ec.eurostat.jgiscotools.io.CSVUtil;
@@ -26,6 +27,9 @@ public class ZZZJoin2021Population {
 	public static void main(String[] args) throws Exception {
 		logger.info("Start");
 
+		//get crs
+		CoordinateReferenceSystem crs = GeoData.getCRS(basePath+"in/grid_100km_point.gpkg");
+
 		for(int resKM : resKMs) {
 
 			logger.info("Load 2021 population");
@@ -41,17 +45,39 @@ public class ZZZJoin2021Population {
 				pop2021Ind.put(d.get("GRD_ID"), p);
 			}
 
+			//System.out.println(pop2021Ind);
 
 			for(String gt : new String[]{"surf", "point"}) {
-				logger.info("res " + resKM + " " + gt);
+				logger.info("res " + resKM + "km " + gt);
 
-				logger.info("load intial GPKG ");
+				logger.info("load intial GPKG");
 				Collection<Feature> fs = GeoData.getFeatures(basePath+"in/grid_"+resKM+"km_"+gt+".gpkg");
 				logger.info(fs.size());
+				//System.out.println(fs.iterator().next().getAttributes().keySet());
+				//[DIST_BORD, TOT_P_2018, TOT_P_2006, GRD_ID, TOT_P_2011, Y_LLC, CNTR_ID, NUTS2016_3, NUTS2016_2, NUTS2016_1, NUTS2016_0, LAND_PC, X_LLC, NUTS2021_3, NUTS2021_2, DIST_COAST, NUTS2021_1, NUTS2021_0]
 
+				logger.info("Add 2021 population");
+				for(Feature f : fs) {
+					//get cell id
+					String gid = (String)f.getAttribute("GRD_ID");
+					if(gid == null || gid.isEmpty())
+						System.err.println("No grid id");
+
+					//get 2021 population using index
+					Integer p = pop2021Ind.get(gid);
+
+					//add new attribute
+					f.setAttribute("TOT_P_2021", p==null?0 : p.intValue());
+				}
+
+				logger.info(fs.size());
+				//System.out.println(fs.iterator().next().getAttributes().keySet());
+
+				GeoData.save(fs, basePath+"out/grid_"+resKM+"km_"+gt+".gpkg", crs);
 			}
 		}
 
+		logger.info("End");
 	}
 
 }
