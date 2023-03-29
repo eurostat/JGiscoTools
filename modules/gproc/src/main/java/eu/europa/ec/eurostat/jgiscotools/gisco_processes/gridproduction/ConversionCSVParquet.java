dc78d85054e940215b9d89d9d1fbebd621b60654
jgaffuri <jgaf@proton.me>
2023-03-29 16:09:23 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridproduction/ConversionCSVParquet.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridproduction/ConversionCSVParquet.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
+import eu.europa.ec.eurostat.jgiscotools.ParquetUtil;
 import eu.europa.ec.eurostat.jgiscotools.feature.Feature;
 import eu.europa.ec.eurostat.jgiscotools.io.CSVUtil;
 import eu.europa.ec.eurostat.jgiscotools.io.geo.GeoData;
@@ -30,21 +32,34 @@ public class ConversionCSVParquet {
 		CoordinateReferenceSystem crs = GeoData.getCRS(basePath+"in/grid_100km_point.gpkg");
 
 		for(int resKM : resKMs) {
-			for(String gt : new String[]{"surf", "point"}) {
-				logger.info("res " + resKM + "km " + gt);
+			logger.info("res " + resKM + "km");
 
-				logger.info("load intial GPKG");
-				Collection<Feature> fs = GeoData.getFeatures(basePath+"in/grid_"+resKM+"km_"+gt+".gpkg");
-				logger.info(fs.size());
-				//System.out.println(fs.iterator().next().getAttributes().keySet());
-				//[DIST_BORD, TOT_P_2018, TOT_P_2006, GRD_ID, TOT_P_2011, Y_LLC, CNTR_ID, NUTS2016_3, NUTS2016_2, NUTS2016_1, NUTS2016_0, LAND_PC, X_LLC, NUTS2021_3, NUTS2021_2, DIST_COAST, NUTS2021_1, NUTS2021_0]
+			logger.info("load intial GPKG");
+			Collection<Feature> fs = GeoData.getFeatures(basePath+"in/grid_"+resKM+"km_point.gpkg");
+			logger.info(fs.size());
+			//System.out.println(fs.iterator().next().getAttributes().keySet());
+			//[DIST_BORD, TOT_P_2018, TOT_P_2006, GRD_ID, TOT_P_2011, Y_LLC, CNTR_ID, NUTS2016_3, NUTS2016_2, NUTS2016_1, NUTS2016_0, LAND_PC, X_LLC, NUTS2021_3, NUTS2021_2, DIST_COAST, NUTS2021_1, NUTS2021_0]
 
-				logger.info("prepare CSV");
-				
-
-
-				//GeoData.save(fs, basePath+"out/grid_"+resKM+"km_"+gt+".gpkg", crs);
+			logger.info("prepare CSV");
+			Collection<Map<String, String>> data = new ArrayList<>();
+			for(Feature f : fs) {
+				HashMap<String,String> d = new HashMap<>();
+				//copy attributes
+				for(Entry<String, Object> att : f.getAttributes().entrySet())
+					d.put(att.getKey(), att.getValue().toString());
+				//release a bit memory progressively
+				f.getAttributes().clear();
+				f.setGeometry(null);
 			}
+			fs.clear(); fs = null;
+			logger.info(data.size());
+
+			logger.info("save CSV");
+			CSVUtil.save(data, basePath+"in/grid_"+resKM+"km.csv");
+			data.clear(); data = null;
+
+			logger.info("convert to parquet");
+			ParquetUtil.convertCSVToParquet(basePath+"in/grid_"+resKM+"km.csv", basePath+"in/", "grid_"+resKM+"km", "GZIP");
 		}
 
 		logger.info("End");
