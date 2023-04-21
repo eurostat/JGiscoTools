8961a3e9639779002643b60c766969d5e9e58f6f
jgaffuri <jgaf@proton.me>
2023-04-21 09:41:57 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/adminValidation/TopologyCheck.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/adminValidation/TopologyCheck.java
@@ -14,12 +14,11 @@ public class TopologyCheck {
 		String inFolder = "/home/juju/Bureau/gisco/adm_validity_check/";
 		String outFolder = "/home/juju/Bureau/gisco/adm_validity_check/";
 
-
 		//SimpleFeatureType sc = GeoData.getSchema(inFolder + "2023_"+t+".gpkg");
-		ArrayList<Feature> fs = GeoData.getFeatures(inFolder + "2023_"+t+".gpkg", "inspireId");
+		ArrayList<Feature> fs = GeoData.getFeatures(inFolder + "OSM_ALL.gpkg", "inspireId");
 		System.out.println(fs.size());
 
-		TesselationQuality.checkQuality(fs, 1, outFolder + "topocheck_"+t+".csv", true, false, 100000, 20000, true);
+		TesselationQuality.checkQuality(fs, 1, outFolder + "topocheck_OSM_ALL.csv", true, false, 100000, 20000, true);
 
 		System.out.println("End");
 	}
