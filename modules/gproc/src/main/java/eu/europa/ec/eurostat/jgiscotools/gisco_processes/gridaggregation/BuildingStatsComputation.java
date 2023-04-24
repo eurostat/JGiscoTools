823e7b7e0e7a8a01b0e4529085d9f7315fa32ea7
jgaffuri <jgaf@proton.me>
2023-04-24 12:17:48 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
@@ -51,7 +51,7 @@ public class BuildingStatsComputation {
 		Collection<Feature> fs = null;
 		for(String dep : new String[] { "057" }) {
 			logger.info("   "+dep);
-			ArrayList<Feature> fs_ = GeoData.getFeatures(basePath + "geodata/fr/bdtopo/" + dep + "/BATI/BATIMENT.shp"); //, null, fil);
+			ArrayList<Feature> fs_ = GeoData.getFeatures(basePath + "geodata/fr/bdtopo/" + dep + "/BATIMENT.gpkg"); //, null, fil);
 			if(fs == null) fs = fs_; else fs.addAll( fs_ );
 			logger.info(fs.size() + " buildings");
 		}
