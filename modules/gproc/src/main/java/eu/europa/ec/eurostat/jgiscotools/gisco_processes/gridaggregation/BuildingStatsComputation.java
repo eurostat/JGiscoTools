d3ef7b12f0559e57e4998429afb0db98f7bd459e
jgaffuri <jgaf@proton.me>
2023-04-26 15:39:20 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/gridaggregation/BuildingStatsComputation.java
@@ -223,23 +223,27 @@ public class BuildingStatsComputation {
 
 			BuildingStat bs = new BuildingStat();
 
-			//Object n = f.getAttribute("NATURE");
-			String nS = f.getAttribute("NATURE").toString();
-			if("0".equals(nS)) bs.res = contrib;
-			else if(nS.subSequence(0, 1).equals("1")) bs.indus = contrib;
-			else if(nS.subSequence(0, 1).equals("2")) bs.agri = contrib;
-			else if(nS.subSequence(0, 1).equals("3")) bs.commServ = contrib;
-			else if( "41206".equals(nS) || "41207".equals(nS) || "41208".equals(nS) ) bs.res = contrib;
-			else if(nS.subSequence(0, 1).equals("4")) bs.commServ = contrib;
-			else if(nS.subSequence(0, 1).equals("5")) bs.commServ = contrib;
-			else if("60000".equals(nS)) {}
-			else if(nS.subSequence(0, 1).equals("7")) bs.commServ = contrib;
-			else if("80000".equals(nS)) bs.agri = contrib;
-			else if("90000".equals(nS)) {}
-			else if("100000".equals(nS)) {}
-			else {
-				System.err.println(nS);
+			Object n = f.getAttribute("NATURE");
+			if(n==null) {
 				bs.res = contrib;
+			} else {
+				String nS = f.getAttribute("NATURE").toString();
+				if("0".equals(nS)) bs.res = contrib;
+				else if(nS.subSequence(0, 1).equals("1")) bs.indus = contrib;
+				else if(nS.subSequence(0, 1).equals("2")) bs.agri = contrib;
+				else if(nS.subSequence(0, 1).equals("3")) bs.commServ = contrib;
+				else if( "41206".equals(nS) || "41207".equals(nS) || "41208".equals(nS) ) bs.res = contrib;
+				else if(nS.subSequence(0, 1).equals("4")) bs.commServ = contrib;
+				else if(nS.subSequence(0, 1).equals("5")) bs.commServ = contrib;
+				else if("60000".equals(nS)) {}
+				else if(nS.subSequence(0, 1).equals("7")) bs.commServ = contrib;
+				else if("80000".equals(nS)) bs.agri = contrib;
+				else if("90000".equals(nS)) {}
+				else if("100000".equals(nS)) {}
+				else {
+					System.err.println(nS);
+					bs.res = contrib;
+				}
 			}
 
 			return bs;
