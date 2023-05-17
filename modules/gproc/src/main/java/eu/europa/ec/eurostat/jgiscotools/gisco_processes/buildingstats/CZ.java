9fda3101bb1b37d3787ecb1d556347ec20f3a3d4
jgaffuri <jgaf@proton.me>
2023-05-17 15:07:18 +0200

    a

---
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/buildingstats/CZ.java
@@ -0,0 +1,9 @@
+package eu.europa.ec.eurostat.jgiscotools.gisco_processes.buildingstats;
+
+public class CZ {
+
+	//see zabaged database
+	//https://geoportal.cuzk.cz/Dokumenty/KATALOG_OBJEKTU_ZABAGED_2020.pdf
+	//https://geoportal.cuzk.cz/(S(3ffuh4dxpwizuhphz1z0m4th))/Default.aspx?mode=TextMeta&text=dSady_zabaged&side=zabaged&menu=24
+	
+}
