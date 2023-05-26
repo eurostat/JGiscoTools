a7ddfaf014f0ea9e78fdab7cded6c2b843604902
jgaffuri <jgaf@proton.me>
2023-05-26 09:11:56 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/buildingstats/cnt/CZ.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/buildingstats/cnt/CZ.java
@@ -5,5 +5,36 @@ public class CZ {
 	//see zabaged database
 	//https://geoportal.cuzk.cz/Dokumenty/KATALOG_OBJEKTU_ZABAGED_2020.pdf
 	//https://geoportal.cuzk.cz/(S(3ffuh4dxpwizuhphz1z0m4th))/Default.aspx?mode=TextMeta&text=dSady_zabaged&side=zabaged&menu=24
+
+	
+	
+	/*
+	ZABAGED
+
+	WFS download service - ZABAGED®
+	https://geoportal.cuzk.cz/wfs_zbg/wfservice.aspx
+	ArcGIS rest
+	https://ags.cuzk.cz/arcgis/rest/services
+
+	Or from
+	https://geoportal.cuzk.cz/(S(u4at4rnojt4fcglyluffow3b))/Default.aspx?mode=eShop&head_tab=sekce-01-gp&menu=13#
+	but have to pay
+
+
+
+	inspire
+
+	https://services.cuzk.cz/gml/inspire/bu/epsg-4258/
+	https://services.cuzk.cz/gml/inspire/bu/
+
+	XML version:
+	https://atom.cuzk.cz/getservicefeed.ashx?service=BU
+
+
+
+
+	https://geoportal.cuzk.cz/(S(0pmz4jeja4gtkswkdoljwfng))/Default.aspx?lng=CZ&mode=TextMeta&side=INSPIRE_dSady&metadataID=CZ-00025712-CUZK_SERIES-MD_BU
+*/
+
 	
 }
