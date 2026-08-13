cf30d23c6d8ca72809b66834516fc59760a8b682
jgaffuri <jgaf@proton.me>
2026-08-13 22:55:01 +0200

    a

---
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/csvMultiResolution/Proc.java
@@ -0,0 +1,7 @@
+package eu.europa.ec.eurostat.jgiscotools.gisco_processes.csvMultiResolution;
+
+public class Proc {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
