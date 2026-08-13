05245f7fc7709438ab3fc1fb239753191169a079
jgaffuri <jgaf@proton.me>
2026-08-14 00:10:44 +0200

    a

--- modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/csvMultiResolution/Proc.java
+++ modules/gproc/src/main/java/eu/europa/ec/eurostat/jgiscotools/gisco_processes/csvMultiResolution/Proc.java
@@ -1,7 +1,5 @@
 package eu.europa.ec.eurostat.jgiscotools.gisco_processes.csvMultiResolution;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
